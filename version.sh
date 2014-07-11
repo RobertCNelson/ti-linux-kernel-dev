1b550fdd0122be7c53799a0c09ab3d6a2d86b5e8
Robert Nelson <robertcnelson@gmail.com>
2014-07-10 20:57:30 -0500

    3.14.12-ti-r2 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,8 +26,8 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.14
-KERNEL_TAG=${KERNEL_REL}.11
-BUILD=ti-r1
+KERNEL_TAG=${KERNEL_REL}.12
+BUILD=ti-r2
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
