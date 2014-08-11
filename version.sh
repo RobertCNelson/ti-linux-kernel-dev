f2c99fefd07cfae64187450c2b6e5245c64ba632
Robert Nelson <robertcnelson@gmail.com>
2014-08-11 11:18:08 -0500

    3.14.16-ti-r8 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,8 +26,8 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.14
-KERNEL_TAG=${KERNEL_REL}.15
-BUILD=ti-r7.1
+KERNEL_TAG=${KERNEL_REL}.16
+BUILD=ti-r8
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
