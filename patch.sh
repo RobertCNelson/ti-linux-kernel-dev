6260ac44d001cbca2cafe575b632c995ea646aa7
Robert Nelson <robertcnelson@gmail.com>
2014-08-12 17:02:08 -0500

    bbb: merge in pinmuxing
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -67,6 +67,48 @@ local_patch () {
 external_git
 #local_patch
 
+pinmux () {
+	echo "dir: pinmux"
+#start_cleanup
+	# cp arch/arm/boot/dts/am335x-bone-common.dtsi arch/arm/boot/dts/am335x-bone-common-pinmux.dtsi
+	# gedit arch/arm/boot/dts/am335x-bone-common.dtsi arch/arm/boot/dts/am335x-bone-common-pinmux.dtsi &
+	# gedit arch/arm/boot/dts/am335x-boneblack.dts arch/arm/boot/dts/am335x-bone.dts &
+	# git add arch/arm/boot/dts/am335x-bone-common-pinmux.dtsi
+	# git commit -a -m 'am335x-bone-common: split out am33xx_pinmux' -s
+
+	${git} "${DIR}/patches/pinmux/0001-am335x-bone-common-split-out-am33xx_pinmux.patch"
+
+	# meld arch/arm/boot/dts/am335x-bone-common-pinmux.dtsi arch/arm/boot/dts/am335x-boneblack.dts
+	# git commit -a -m 'am335x-boneblack: split out am33xx_pinmux' -s
+
+	${git} "${DIR}/patches/pinmux/0002-am335x-boneblack-split-out-am33xx_pinmux.patch"
+
+	# cp arch/arm/boot/dts/am335x-boneblack.dts arch/arm/boot/dts/am335x-boneblack-emmc.dtsi
+	# gedit arch/arm/boot/dts/am335x-boneblack.dts arch/arm/boot/dts/am335x-boneblack-emmc.dtsi &
+	# git add arch/arm/boot/dts/am335x-boneblack-emmc.dtsi
+	# git commit -a -m 'am335x-boneblack: split out emmc' -s
+
+	${git} "${DIR}/patches/pinmux/0003-am335x-boneblack-split-out-emmc.patch"
+
+	# cp arch/arm/boot/dts/am335x-boneblack.dts arch/arm/boot/dts/am335x-boneblack-nxp-hdmi.dtsi
+	# gedit arch/arm/boot/dts/am335x-boneblack.dts arch/arm/boot/dts/am335x-boneblack-nxp-hdmi.dtsi &
+	# git add arch/arm/boot/dts/am335x-boneblack-nxp-hdmi.dtsi
+	# git commit -a -m 'am335x-boneblack: split out nxp hdmi' -s
+
+	${git} "${DIR}/patches/pinmux/0004-am335x-boneblack-split-out-nxp-hdmi.patch"
+
+	${git} "${DIR}/patches/pinmux/0005-am335x-bone-common-pinmux-i2c2.patch"
+	${git} "${DIR}/patches/pinmux/0006-am335x-bone-common-pinmux-uart.patch"
+	${git} "${DIR}/patches/pinmux/0007-am335x-bone-common-pinmux-spi.patch"
+	${git} "${DIR}/patches/pinmux/0008-am335x-bone-common-pinmux-mcasp0.patch"
+	${git} "${DIR}/patches/pinmux/0009-am335x-bone-common-pinmux-lcd4.patch"
+#number=9
+#cleanup
+}
+
+###
+pinmux
+
 packaging_setup () {
 	cp -v "${DIR}/3rdparty/packaging/builddeb" "${DIR}/KERNEL/scripts/package"
 	git commit -a -m 'packaging: sync with mainline' -s
--- version.sh
+++ version.sh
@@ -27,7 +27,7 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 #Kernel/Build
 KERNEL_REL=3.14
 KERNEL_TAG=${KERNEL_REL}.16
-BUILD=ti-r8.1
+BUILD=ti-r8.2
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
