009760411cad24ef6e9e4dcc0020e5574a6c9067
Robert Nelson <robertcnelson@gmail.com>
2014-11-21 12:21:29 -0600

    ti: merge to: http://git.ti.com/gitweb/?p=ti-linux-kernel/ti-linux-kernel.git;a=commit;h=5dcfe16d0585aeed7158820e934a5e43ff017476
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -26,8 +26,8 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.14
-KERNEL_TAG=${KERNEL_REL}.24
-BUILD=ti-r36.2
+KERNEL_TAG=${KERNEL_REL}.25
+BUILD=ti-r36.3
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
