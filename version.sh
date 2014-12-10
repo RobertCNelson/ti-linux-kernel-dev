de8b4101ecf8c1bbef6cf6478e9c498bfd2f9434
Robert Nelson <robertcnelson@gmail.com>
2014-12-10 12:41:24 -0600

    cape: bbb-exp-c: is really 1024x600
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -27,7 +27,7 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 #Kernel/Build
 KERNEL_REL=3.14
 KERNEL_TAG=${KERNEL_REL}.26
-BUILD=ti-xenomai-r39.3
+BUILD=ti-xenomai-r39.4
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
