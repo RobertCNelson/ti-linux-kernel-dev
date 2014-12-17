7fed2544349dd83e3f691ec36046d0132f731cfb
Robert Nelson <robertcnelson@gmail.com>
2014-12-17 15:26:02 -0600

    config: debian network/wireless
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -30,7 +30,7 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 #Kernel/Build
 KERNEL_REL=3.14
 KERNEL_TAG=${KERNEL_REL}.26
-BUILD=${build_prefix}-r41.5
+BUILD=${build_prefix}-r41.6
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
