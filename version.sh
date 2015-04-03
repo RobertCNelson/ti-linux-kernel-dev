b4ae7f1b2536c8f28f42a80d124154d570e111e8
Robert Nelson <robertcnelson@gmail.com>
2015-04-03 14:09:49 -0500

    3.14.37-ti-xenomai-r57 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 #Kernel/Build
 KERNEL_REL=3.14
 KERNEL_TAG=${KERNEL_REL}.37
-BUILD=${build_prefix}-r56.2
+BUILD=${build_prefix}-r57
 
 #v3.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
