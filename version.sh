97a6c25b9ed404ddc0ea858d04fbb69df5dc4d67
Robert Nelson <robertcnelson@gmail.com>
2015-04-30 11:35:58 -0500

    3.14.40-ti-r62 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -17,7 +17,7 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 #Kernel/Build
 KERNEL_REL=3.14
 KERNEL_TAG=${KERNEL_REL}.40
-BUILD=${build_prefix}-r61.5
+BUILD=${build_prefix}-r62
 
 #v3.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
