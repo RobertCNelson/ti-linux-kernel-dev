ea7b4a4957f90ec1b1691adea5caeaa1bf716c09
Robert Nelson <robertcnelson@gmail.com>
2015-05-22 15:11:39 -0500

    merge ti: https://github.com/RobertCNelson/ti-linux-kernel/compare/d0f5c460aac292d2942b23dd6199fe23021212ad...c46632d93c5e1d56bb79748e9bcf6f949c851d9f
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -17,7 +17,7 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 #Kernel/Build
 KERNEL_REL=3.14
 KERNEL_TAG=${KERNEL_REL}.43
-BUILD=${build_prefix}-r65
+BUILD=${build_prefix}-r65.1
 
 #v3.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
@@ -29,7 +29,7 @@ BRANCH="ti-linux-${branch_prefix}3.14.y"
 DISTRO=cross
 DEBARCH=armhf
 
-ti_git_old_release="7763988e04f12b089283e385f3593e7f34a92068"
-ti_git_pre="7763988e04f12b089283e385f3593e7f34a92068"
-ti_git_post="d0f5c460aac292d2942b23dd6199fe23021212ad"
+ti_git_old_release="d0f5c460aac292d2942b23dd6199fe23021212ad"
+ti_git_pre="d0f5c460aac292d2942b23dd6199fe23021212ad"
+ti_git_post="c46632d93c5e1d56bb79748e9bcf6f949c851d9f"
 #
