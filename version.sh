ed9d2d18e1484215b15bf2a18d3bf6fbd0a9bef0
Robert Nelson <robertcnelson@gmail.com>
2015-06-19 10:19:27 -0500

    first pass on ti-4.1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -11,25 +11,25 @@ branch_prefix=""
 #toolchain="gcc_linaro_eabi_4_9"
 #toolchain="gcc_linaro_gnueabi_4_6"
 #toolchain="gcc_linaro_gnueabihf_4_7"
-toolchain="gcc_linaro_gnueabihf_4_8"
-#toolchain="gcc_linaro_gnueabihf_4_9"
+#toolchain="gcc_linaro_gnueabihf_4_8"
+toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
-KERNEL_REL=3.14
-KERNEL_TAG=${KERNEL_REL}.43
-BUILD=${build_prefix}-r68
+KERNEL_REL=4.1
+KERNEL_TAG=${KERNEL_REL}-rc8
+BUILD=${build_prefix}-r0.1
 
 #v3.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
 #KERNEL_SHA=""
 
 #git branch
-BRANCH="ti-linux-${branch_prefix}3.14.y"
+BRANCH="ti-linux-${branch_prefix}4.1.y"
 
 DISTRO=cross
 DEBARCH=armhf
 
-ti_git_old_release="c46632d93c5e1d56bb79748e9bcf6f949c851d9f"
-ti_git_pre="d4d72d56fdc67603633f0168073713180934dbc0"
-ti_git_post="e19ba3d996f22ad8cc7187b30c18347aba0d594d"
+ti_git_old_release=""
+ti_git_pre=""
+ti_git_post=""
 #
