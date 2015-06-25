7c4cef31aaf9bfe8e8fca03468df9e3a47b79586
Robert Nelson <robertcnelson@gmail.com>
2015-06-25 13:23:45 -0500

    3.14.45-ti-r69 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -17,7 +17,7 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 #Kernel/Build
 KERNEL_REL=3.14
 KERNEL_TAG=${KERNEL_REL}.45
-BUILD=${build_prefix}-r68.1
+BUILD=${build_prefix}-r69
 
 #v3.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
