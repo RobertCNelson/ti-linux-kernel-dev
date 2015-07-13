88e330d6d76d4411842070f6874eb75de8dfc7d5
Robert Nelson <robertcnelson@gmail.com>
2015-07-13 15:12:51 -0500

    3.14.48-ti-r72 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -17,7 +17,7 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 #Kernel/Build
 KERNEL_REL=3.14
 KERNEL_TAG=${KERNEL_REL}.48
-BUILD=${build_prefix}-r71.1
+BUILD=${build_prefix}-r72
 
 #v3.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
