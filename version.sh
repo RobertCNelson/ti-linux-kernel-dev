d55781b387dd975fba6a44019307eade2272ced8
Robert Nelson <robertcnelson@gmail.com>
2015-07-16 13:52:33 -0500

    4.1.2-ti-r4 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -17,7 +17,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.2
-BUILD=${build_prefix}-r3.2
+BUILD=${build_prefix}-r4
 
 #v3.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
