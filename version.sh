03f75078b56e3e2e676b06acd2579d2cdc34ca2f
Robert Nelson <robertcnelson@gmail.com>
2015-08-03 15:30:38 -0500

    3.14.49-ti-xenomai-r73 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -17,7 +17,7 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 #Kernel/Build
 KERNEL_REL=3.14
 KERNEL_TAG=${KERNEL_REL}.49
-BUILD=${build_prefix}-r72.1
+BUILD=${build_prefix}-r73
 
 #v3.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
