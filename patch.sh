cf4d4833bc58da03be28441b023e2771723df505
Robert Nelson <robertcnelson@gmail.com>
2015-08-10 07:40:00 -0500

    add musb revert and enable a few more usb configs
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -152,9 +152,11 @@ reverts () {
 	#${git} "${DIR}/patches/reverts/0001-Revert-ARM-dts-am335x-boneblack-disable-RTC-only-sle.patch"
 
 	${git} "${DIR}/patches/reverts/0002-Revert-spi-spidev-Warn-loudly-if-instantiated-from-D.patch"
+	#am335x causing random reboots...
+	${git} "${DIR}/patches/reverts/0003-Revert-usb-musb-dsps-just-start-polling-already.patch"
 
 	if [ "x${regenerate}" = "xenable" ] ; then
-		number=2
+		number=3
 		cleanup
 	fi
 }
