6c23725dfe20feec55de238cc37d2fec2152a9ed
Robert Nelson <robertcnelson@gmail.com>
2015-09-10 13:32:10 -0500

    4.1.6-ti-rt-r15 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -17,7 +17,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.6
-BUILD=${build_prefix}-r14.3
+BUILD=${build_prefix}-r15
 kernel_rt=".5-rt5"
 
 #v3.X-rcX + upto SHA
