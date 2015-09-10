4d95937475f3aea632ea19ba19593656a57604b1
Robert Nelson <robertcnelson@gmail.com>
2015-09-10 09:45:32 -0500

    merge ti: https://github.com/RobertCNelson/ti-linux-kernel/compare/77fdbd06b0cbb428469fdfc6d4a1b10b01f45446...065c5f0eb8b7aacc9d1ef201a1dd6af7f10afd64
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -491,9 +491,9 @@ beaglebone () {
 	#http://git.ti.com/gitweb/?p=ti-cm3-pm-firmware/amx3-cm3.git;a=summary
 	#git clone git://git.ti.com/ti-cm3-pm-firmware/amx3-cm3.git
 	#cd amx3-cm3/
-	#git checkout origin/next-upstream -b tmp
+	#git checkout origin/ti-v4.1.y -b tmp
 
-	#commit 277eef8611e260a5d73a9e3773fff8f767fe2b01
+	#commit 730f0695ca2dda65abcff5763e8f108517bc0d43
 	#Author: Dave Gerlach <d-gerlach@ti.com>
 	#Date:   Wed Mar 4 21:34:54 2015 -0600
 	#
@@ -502,6 +502,7 @@ beaglebone () {
 	#    This version, 0x191, includes the following changes:
 	#         - Add trace output on boot for kernel remoteproc driver
 	#         - Fix resouce table as RSC_INTMEM is no longer used in kernel
+	#         - Add header dependency checking
 	#    
 	#    Signed-off-by: Dave Gerlach <d-gerlach@ti.com>
 
