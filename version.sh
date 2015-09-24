d73168d257335357541ea52f940c6b2390decbd5
Robert Nelson <robertcnelson@gmail.com>
2015-09-24 09:59:52 -0500

    config: drop CONFIG_UIO_PRUSS, as it compets with CONFIG_PRUSS_REMOTEPROC
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -17,7 +17,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.6
-BUILD=${build_prefix}-r16.4
+BUILD=${build_prefix}-r16.5
 kernel_rt=".5-rt5"
 
 #v4.X-rcX + upto SHA
