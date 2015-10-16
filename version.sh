09e5456111b9e90bace5fe2616bf40701055b157
Robert Nelson <robertcnelson@gmail.com>
2015-10-16 12:24:25 -0500

    4.1.10-ti-r23 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -19,7 +19,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.10
-BUILD=${build_prefix}-r22.1
+BUILD=${build_prefix}-r23
 kernel_rt=".7-rt8"
 
 #v4.X-rcX + upto SHA
