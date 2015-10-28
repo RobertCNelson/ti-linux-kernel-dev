8a2c267ecad83331a24008e743b2447969abcf7b
Robert Nelson <robertcnelson@gmail.com>
2015-10-28 11:02:29 -0500

    4.1.12-ti-git20151028-28fa6387 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -19,7 +19,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.12
-BUILD=${build_prefix}-git20151027-1f4e4412
+BUILD=${build_prefix}-git20151028-28fa6387
 kernel_rt=".10-rt10"
 
 #v4.X-rcX + upto SHA
@@ -32,7 +32,7 @@ BRANCH="ti-linux-${branch_prefix}4.1.y"
 DISTRO=cross
 DEBARCH=armhf
 
-ti_git_old_release="53be9f846e382ca613ea8ebbd5316fa78c2dd7fe"
-ti_git_pre="53be9f846e382ca613ea8ebbd5316fa78c2dd7fe"
-ti_git_post="1f4e4412f55812b33ba0c288ea5fb9ec45a0d5ce"
+ti_git_old_release="1f4e4412f55812b33ba0c288ea5fb9ec45a0d5ce"
+ti_git_pre="1f4e4412f55812b33ba0c288ea5fb9ec45a0d5ce"
+ti_git_post="28fa6387eb863d32663cd405eb7f39a74a92c9f6"
 #
