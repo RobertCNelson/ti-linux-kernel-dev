5e7581e7c263aa10db56563ab96ce75921565e5a
Robert Nelson <robertcnelson@gmail.com>
2015-11-05 18:33:16 -0600

    4.1.12-ti-rt-r28 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -19,7 +19,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.12
-BUILD=${build_prefix}-r27.5
+BUILD=${build_prefix}-r28
 kernel_rt=".10-rt11"
 
 #v4.X-rcX + upto SHA
