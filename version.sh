f308b43de7cd36989e2579a9e2613c812895ee83
Robert Nelson <robertcnelson@gmail.com>
2015-11-10 08:59:43 -0600

    kernel bump: v3.14.57
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -18,8 +18,8 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.14
-KERNEL_TAG=${KERNEL_REL}.56
-BUILD=${build_prefix}-r78.2
+KERNEL_TAG=${KERNEL_REL}.57
+BUILD=${build_prefix}-r78.3
 kernel_rt=".53-rt54"
 
 #v4.X-rcX + upto SHA
@@ -33,6 +33,6 @@ DISTRO=cross
 DEBARCH=armhf
 
 ti_git_old_release="b75394168a3a310b0dc2a27de9fb5d0645c12b8d"
-ti_git_pre="3781189918e0713e8af67ac71aa92ed18358a56d"
-ti_git_post="b59fab0e742fff7f404894156445d1d7b695692e"
+        ti_git_pre="b59fab0e742fff7f404894156445d1d7b695692e"
+       ti_git_post="30976c143d2126801398a6ef91cf8fb1ddbe00c0"
 #
