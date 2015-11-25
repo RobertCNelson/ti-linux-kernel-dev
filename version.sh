bf03b18bd1cb72dd3094d80a5491a10ab7b74527
Robert Nelson <robertcnelson@gmail.com>
2015-11-25 12:04:01 -0600

    sync: am335x-boneblack-ttyS2.dtb from dtb-rebuilder
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -24,7 +24,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.13
-BUILD=${build_prefix}-r33.1
+BUILD=${build_prefix}-r33.2
 kernel_rt=".12-rt13"
 
 #v4.X-rcX + upto SHA
