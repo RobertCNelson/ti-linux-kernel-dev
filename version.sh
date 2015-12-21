11171a8254af9c27c964147ed9c05adf8fe43045
Robert Nelson <robertcnelson@gmail.com>
2015-12-21 13:54:37 -0600

    cleanup: tieqep.c
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -24,7 +24,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.13
-BUILD=${build_prefix}-r36
+BUILD=${build_prefix}-r36.1
 kernel_rt=".12-rt13"
 
 #v4.X-rcX + upto SHA
