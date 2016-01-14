15fc0a9adf547992fe21a704c64c1e752035e007
Robert Nelson <robertcnelson@gmail.com>
2016-01-14 13:21:26 -0600

    cleanup/merge with 4.4.0-bone
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -24,7 +24,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}
-BUILD=${build_prefix}-r3.2
+BUILD=${build_prefix}-r3.3
 kernel_rt="-rc6-rt1"
 
 #v4.X-rcX + upto SHA
