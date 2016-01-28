eeb6196f0185dda0accd922c6868f9ee94058f23
Robert Nelson <robertcnelson@gmail.com>
2016-01-28 10:56:22 -0600

    merge from linux-dev
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -4,8 +4,9 @@ ARCH=$(uname -m)
 
 config="omap2plus_defconfig"
 
-build_prefix="ti-rt"
-branch_prefix="rt-"
+build_prefix="ti-rt-r"
+branch_prefix="ti-linux-rt-"
+branch_postfix=".y"
 
 #arm
 KERNEL_ARCH=arm
@@ -24,15 +25,15 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}
-BUILD=${build_prefix}-r5.1
-kernel_rt="-rt2"
+BUILD=${build_prefix}5.2
+kernel_rt="-rt3"
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
 #KERNEL_SHA=""
 
 #git branch
-BRANCH="ti-linux-${branch_prefix}4.4.y"
+BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 
 DISTRO=cross
 DEBARCH=armhf
