94ec0a19439f56d70a80c0bec5050b062002082d
Robert Nelson <robertcnelson@gmail.com>
2016-03-08 11:31:16 -0600

    ti: switch over to the ti-rt-linux-4.4.y branch vs ti-linux-4.4.y
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -70,7 +70,7 @@ cherrypick () {
 }
 
 external_git () {
-	git_tag="ti-linux-4.4.y"
+	git_tag="ti-rt-linux-4.4.y"
 	echo "pulling: ${git_tag}"
 	git pull ${git_opts} ${git_patchset} ${git_tag}
 	#. ${DIR}/.CC
@@ -157,9 +157,6 @@ rt () {
 	rt_patch="${KERNEL_REL}${kernel_rt}"
 	#regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
-		wget -c https://www.kernel.org/pub/linux/kernel/projects/rt/${KERNEL_REL}/patch-${rt_patch}.patch.xz
-		xzcat patch-${rt_patch}.patch.xz | patch -p1 || rt_cleanup
-		rm -f patch-${rt_patch}.patch.xz
 		rm -f localversion-rt
 		git add .
 		git commit -a -m 'merge: CONFIG_PREEMPT_RT Patch Set' -s
