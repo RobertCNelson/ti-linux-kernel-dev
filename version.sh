a4434c8447c676ce7c49b20718c775e22b42dd31
Robert Nelson <robertcnelson@gmail.com>
2016-03-09 16:01:14 -0600

    config: disable CONFIG_DRM_I2C_ADIHDMI (broken again)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,8 +25,8 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.4
-BUILD=${build_prefix}9.3
-#kernel_rt=".3-rt9"
+BUILD=${build_prefix}9.4
+#kernel_rt=""
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
