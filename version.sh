86a72d010e4acf57db36bb4710964e0c173d1ea3
Robert Nelson <robertcnelson@gmail.com>
2016-03-16 15:29:57 -0500

    fix: thumb2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.5
-BUILD=${build_prefix}10.4
+BUILD=${build_prefix}10.5
 #kernel_rt=""
 
 #v4.X-rcX + upto SHA
