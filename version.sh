85e4bde027500ede7305d4292bf42c83fa39c6f3
Robert Nelson <robertcnelson@gmail.com>
2016-03-24 11:39:34 -0500

    4.1.18-ti-rt-r55 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.18
-BUILD=${build_prefix}54.3
+BUILD=${build_prefix}55
 #kernel_rt=""
 
 #v4.X-rcX + upto SHA
@@ -38,7 +38,7 @@ BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 DISTRO=cross
 DEBARCH=armhf
 
-ti_git_old_release="448c88c73751423b736aa5fff21e0bd248b5aa9b"
-        ti_git_pre="a4248d096e1d94ca6e9223c77735ef7c6551ccff"
+ti_git_old_release="3bef1493dbbc61880f09cfda285cd0032a501d41"
+        ti_git_pre="3bef1493dbbc61880f09cfda285cd0032a501d41"
        ti_git_post="3bef1493dbbc61880f09cfda285cd0032a501d41"
 #
