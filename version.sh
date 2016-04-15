2f88c76daf04143cd314bbbea5bb6d4e49a9e5df
Robert Nelson <robertcnelson@gmail.com>
2016-04-15 15:04:31 -0500

    4.4.7-ti-r19 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.7
-BUILD=${build_prefix}18.3
+BUILD=${build_prefix}19
 #kernel_rt=""
 
 #v4.X-rcX + upto SHA
