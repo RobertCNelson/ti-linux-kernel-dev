046dd27892978f77e71ac11f3e25f63aba058275
Robert Nelson <robertcnelson@gmail.com>
2016-05-11 13:41:59 -0500

    4.4.9-ti-r26 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.9
-BUILD=${build_prefix}25.2
+BUILD=${build_prefix}26
 #kernel_rt=""
 
 #v4.X-rcX + upto SHA
