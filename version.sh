07af7f8315f35e1e6725c95f967bf13ef3ffd2a7
Robert Nelson <robertcnelson@gmail.com>
2016-05-24 09:43:35 -0500

    4.1.25-ti-rt-r62 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.25
-BUILD=${build_prefix}61.1
+BUILD=${build_prefix}62
 #kernel_rt=""
 
 #v4.X-rcX + upto SHA
