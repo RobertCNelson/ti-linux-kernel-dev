530963eb4fa35bb9c41517e403b730ff1e7651bd
Robert Nelson <robertcnelson@gmail.com>
2016-05-27 15:59:29 -0500

    4.4.11-ti-rt-r29 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.11
-BUILD=${build_prefix}28.3
+BUILD=${build_prefix}29
 #kernel_rt=""
 
 #v4.X-rcX + upto SHA
