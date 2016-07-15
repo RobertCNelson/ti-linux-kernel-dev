a0f02dd803db55e75fc1bcb22987eeb819f731d7
Robert Nelson <robertcnelson@gmail.com>
2016-07-15 11:31:45 -0500

    x15: keep cmem formatting...
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -1032,9 +1032,10 @@ x15 () {
 	fi
 
 	${git} "${DIR}/patches/x15/fixes/0001-x15-mmc-cmem-debugss.patch"
+	${git} "${DIR}/patches/x15/fixes/0002-x15-cmem-keep-this-formating.patch"
 
 	if [ "x${regenerate}" = "xenable" ] ; then
-		number=1
+		number=2
 		cleanup
 	fi
 }
