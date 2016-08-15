89b8c293b4227303565dbe19f4870b77856bf104
Robert Nelson <robertcnelson@gmail.com>
2016-08-15 14:57:57 -0500

    4.4.17-ti-rt-r40 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.17
-BUILD=${build_prefix}39
+BUILD=${build_prefix}40
 #kernel_rt=""
 
 #v4.X-rcX + upto SHA
@@ -41,7 +41,7 @@ BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 DISTRO=cross
 DEBARCH=armhf
 
-ti_git_old_release="3ad9429aa90ec7e1d351d1cd228d83b4d622e479"
-        ti_git_pre="3ad9429aa90ec7e1d351d1cd228d83b4d622e479"
+ti_git_old_release="74a3cfb6cfb1092f37cd57805f495a4d37a4f068"
+        ti_git_pre="74a3cfb6cfb1092f37cd57805f495a4d37a4f068"
        ti_git_post="74a3cfb6cfb1092f37cd57805f495a4d37a4f068"
 #
