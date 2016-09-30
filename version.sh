a9f95643829de51f53a8909dd9f69e3b35c41d7d
Robert Nelson <robertcnelson@gmail.com>
2016-09-29 19:45:56 -0500

    4.4.22-ti-r50 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.22
-BUILD=${build_prefix}49.1
+BUILD=${build_prefix}50
 #kernel_rt=""
 
 #v4.X-rcX + upto SHA
