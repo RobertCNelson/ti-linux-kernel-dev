6ca7a3fb75b321bb898d4a862e159477e82022c1
Robert Nelson <robertcnelson@gmail.com>
2016-10-03 15:31:44 -0500

    merge: Support for CTAG face2|4 Audio Card
    fixes: https://github.com/beagleboard/linux/pull/104
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.23
-BUILD=${build_prefix}50.1
+BUILD=${build_prefix}50.2
 #kernel_rt=""
 
 #v4.X-rcX + upto SHA
