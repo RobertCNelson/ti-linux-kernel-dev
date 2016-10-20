893788ca7afa5992afb373d0093fc0f728b9ae02
Robert Nelson <robertcnelson@gmail.com>
2016-10-20 09:59:21 -0500

    bbgw: make sure bt_aud_out is low
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.24
-BUILD=${build_prefix}57
+BUILD=${build_prefix}57.1
 #kernel_rt=""
 
 #v4.X-rcX + upto SHA
