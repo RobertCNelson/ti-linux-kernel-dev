d53ca02a9e2a6317311f1737fedd2f72d3c2600a
Robert Nelson <robertcnelson@gmail.com>
2016-11-03 11:27:04 -0500

    config: build-in: CONFIG_TOUCHSCREEN_EDT_FT5X06
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_6"
 #Kernel/Build
 KERNEL_REL=4.9
 KERNEL_TAG=${KERNEL_REL}-rc3
-BUILD=${build_prefix}2
+BUILD=${build_prefix}2.1
 kernel_rt="false"
 #kernel_rt="true"
 
