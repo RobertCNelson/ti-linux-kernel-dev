0a7b4df18a703e2092a4041c29bb1aef81fddeb1
Robert Nelson <robertcnelson@gmail.com>
2016-11-08 10:37:55 -0600

    fix blue
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_6"
 #Kernel/Build
 KERNEL_REL=4.9
 KERNEL_TAG=${KERNEL_REL}-rc4
-BUILD=${build_prefix}2.5
+BUILD=${build_prefix}2.6
 kernel_rt="false"
 #kernel_rt="true"
 
