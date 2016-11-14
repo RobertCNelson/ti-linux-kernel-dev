2a1f9710a9c7aaf65dbc3afec630934668641829
Robert Nelson <robertcnelson@gmail.com>
2016-11-14 10:47:00 -0600

    add brcmfmac: brcmf_sdio_drivestrengthinit
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_6"
 #Kernel/Build
 KERNEL_REL=4.9
 KERNEL_TAG=${KERNEL_REL}-rc5
-BUILD=${build_prefix}3.1
+BUILD=${build_prefix}3.2
 kernel_rt="false"
 #kernel_rt="true"
 
