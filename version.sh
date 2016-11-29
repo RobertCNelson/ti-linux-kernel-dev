bb65e6172b0b88cbbeb96239135cdaa75d0264a5
Robert Nelson <robertcnelson@gmail.com>
2016-11-29 09:11:16 -0600

    4.4.35-ti-xenomai-r70 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.35
-BUILD=${build_prefix}69.1
+BUILD=${build_prefix}70
 #kernel_rt=""
 
 #v4.X-rcX + upto SHA
