eb63eee373c71ad2dde2f4fda48eb72da91b47e0
Robert Nelson <robertcnelson@gmail.com>
2016-12-02 11:23:51 -0600

    4.4.36-ti-rt-r71 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.36
-BUILD=${build_prefix}70.1
+BUILD=${build_prefix}71
 #kernel_rt=""
 
 #v4.X-rcX + upto SHA
