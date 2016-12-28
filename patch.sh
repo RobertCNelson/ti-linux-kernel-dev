039ba975c08daf8a3893f7890e0dc959adc497fe
Robert Nelson <robertcnelson@gmail.com>
2016-12-28 15:43:33 -0600

    and this
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -501,7 +501,6 @@ drivers () {
 	dir 'drivers/ti/eqep'
 	dir 'drivers/ti/mcasp'
 	dir 'drivers/ti/mmc'
-#	dir 'drivers/ti/musb'
 	dir 'drivers/ti/omapdrm'
 	dir 'drivers/ti/rpmsg'
 	dir 'drivers/ti/rtc'
