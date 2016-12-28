1ed490a86d11ab4b8ed70943aec09b1c703f14f8
Robert Nelson <robertcnelson@gmail.com>
2016-12-28 09:50:47 -0600

    4.4.39-ti-r77 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.39
-BUILD=${build_prefix}76.1
+BUILD=${build_prefix}77
 #kernel_rt=""
 
 #v4.X-rcX + upto SHA
