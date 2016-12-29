aa7ad778bd26e3b7f51d7b33738627d21dc240ec
Robert Nelson <robertcnelson@gmail.com>
2016-12-29 11:11:04 -0600

    fix
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 3rdparty/travis/build_on_x86.sh
+++ 3rdparty/travis/build_on_x86.sh
@@ -2,7 +2,7 @@
 
 wget https://releases.linaro.org/components/toolchain/binaries/6.2-2016.11/arm-linux-gnueabihf/gcc-linaro-6.2.1-2016.11-x86_64_arm-linux-gnueabihf.tar.xz
 tar xf gcc-linaro-6.2.1-2016.11-x86_64_arm-linux-gnueabihf.tar.xz
-export CC=`pwd`/gcc-linaro-6.2.1-2016.11-x86_64_arm-linux-gnueabihfbin/arm-linux-gnueabihf-
+export CC=`pwd`/gcc-linaro-6.2.1-2016.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 
 make ARCH=arm bb.org_defconfig
 make ARCH=arm -j4 CROSS_COMPILE="ccache ${CC}" zImage
