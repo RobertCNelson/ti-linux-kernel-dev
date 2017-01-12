ae5581fb188bcf6168d17cd7de495b2f22b13d65
Robert Nelson <robertcnelson@gmail.com>
2017-01-12 16:31:04 -0600

    4.9.3-ti-rt-r14 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_6"
 #Kernel/Build
 KERNEL_REL=4.9
 KERNEL_TAG=${KERNEL_REL}.3
-BUILD=${build_prefix}13.3
+BUILD=${build_prefix}14
 kernel_rt="-rt1"
 
 #v4.X-rcX + upto SHA
