5336ee973e84b982adb571578f84821120d92747
Robert Nelson <robertcnelson@gmail.com>
2017-01-16 13:40:28 -0600

    config: sync with debian
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_6"
 #Kernel/Build
 KERNEL_REL=4.9
 KERNEL_TAG=${KERNEL_REL}.4
-BUILD=${build_prefix}14.2
+BUILD=${build_prefix}14.3
 kernel_rt="-rt1"
 
 #v4.X-rcX + upto SHA
