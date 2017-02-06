b49ce5e944a832f304b2df072b2c56eb272486d8
Robert Nelson <robertcnelson@gmail.com>
2017-02-06 13:52:57 -0600

    4.4.47-ti-rt-r87 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.47
-BUILD=${build_prefix}86.1
+BUILD=${build_prefix}87
 #kernel_rt=""
 
 #v4.X-rcX + upto SHA
