3ca5abf5274ead17b6eff00b9b75429748e5f7cb
Robert Nelson <robertcnelson@gmail.com>
2017-05-22 11:54:37 -0500

    4.4.62-ti-r105 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -29,7 +29,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.62
-BUILD=${build_prefix}104.1
+BUILD=${build_prefix}105
 #kernel_rt=""
 
 #v4.X-rcX + upto SHA
