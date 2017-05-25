b5195cd6b001f28af923a3ed06a063004bb65a9b
Robert Nelson <robertcnelson@gmail.com>
2017-05-25 14:38:04 -0500

    merge ti: https://github.com/RobertCNelson/ti-linux-kernel/compare/260d5e7a6f84473ab910811d5d56cd9a7b87fae9...c5a66910c41ba09e1d3715907d7649d3bd497216
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,9 +28,9 @@ toolchain="gcc_linaro_gnueabihf_5"
 
 #Kernel/Build
 KERNEL_REL=4.4
-KERNEL_TAG=${KERNEL_REL}.62
-BUILD=${build_prefix}105
-#kernel_rt=""
+KERNEL_TAG=${KERNEL_REL}.68
+BUILD=${build_prefix}105.2
+kernel_rt=".66-rt79"
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
@@ -44,10 +44,10 @@ DEBARCH=armhf
 
 ti_git_old_release="260d5e7a6f84473ab910811d5d56cd9a7b87fae9"
         ti_git_pre="260d5e7a6f84473ab910811d5d56cd9a7b87fae9"
-       ti_git_post="260d5e7a6f84473ab910811d5d56cd9a7b87fae9"
+       ti_git_post="c5a66910c41ba09e1d3715907d7649d3bd497216"
 #
 
 #https://git.xenomai.org/xenomai-3.git/
 #https://git.xenomai.org/xenomai-3.git/log/?h=stable-3.0.x
-xenomai_checkout="679b9191f81bc44d05c4eaac28d3d6362c12d689"
+xenomai_checkout="8c0f8161373d483ca827d22056b8fe53e16cf847"
 #
