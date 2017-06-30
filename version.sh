6f22210989430fca3be62fcc25692e53cb0793a3
Robert Nelson <robertcnelson@gmail.com>
2017-06-30 14:19:26 -0500

    merge ti: https://github.com/RobertCNelson/ti-linux-kernel/compare/95b7efc0da1863f691f01008561603c145070f23...eb9eb942e9058b122a9582819523d173715ba36f
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -31,8 +31,8 @@ toolchain="gcc_linaro_gnueabihf_6"
 
 #Kernel/Build
 KERNEL_REL=4.9
-KERNEL_TAG=${KERNEL_REL}.34
-BUILD=${build_prefix}43
+KERNEL_TAG=${KERNEL_REL}.35
+BUILD=${build_prefix}43.1
 kernel_rt=".33-rt23"
 
 #v4.X-rcX + upto SHA
@@ -45,9 +45,9 @@ BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 DISTRO=cross
 DEBARCH=armhf
 
-ti_git_old_release="6347dba3d35f2458875c6ce711d511db8b1e9748"
-        ti_git_pre="6347dba3d35f2458875c6ce711d511db8b1e9748"
-       ti_git_post="95b7efc0da1863f691f01008561603c145070f23"
+ti_git_old_release="95b7efc0da1863f691f01008561603c145070f23"
+        ti_git_pre="95b7efc0da1863f691f01008561603c145070f23"
+       ti_git_post="eb9eb942e9058b122a9582819523d173715ba36f"
 #
 
 #https://git.xenomai.org/xenomai-3.git/
