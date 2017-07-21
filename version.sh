e3043f6b82a812eda32a64e399d7a367a94c82cd
Robert Nelson <robertcnelson@gmail.com>
2017-07-21 10:46:53 -0500

    4.4.68-ti-xenomai-r112 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,7 +32,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.68
-BUILD=${build_prefix}111
+BUILD=${build_prefix}112
 kernel_rt=".66-rt79"
 
 #v4.X-rcX + upto SHA
