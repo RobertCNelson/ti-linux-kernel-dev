395189af46c1401e9ee5667c965c985060845b1a
Robert Nelson <robertcnelson@gmail.com>
2017-07-28 15:33:18 -0500

    4.9.40-ti-r51 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,7 +32,7 @@ toolchain="gcc_linaro_gnueabihf_6"
 #Kernel/Build
 KERNEL_REL=4.9
 KERNEL_TAG=${KERNEL_REL}.40
-BUILD=${build_prefix}50.1
+BUILD=${build_prefix}51
 kernel_rt=".35-rt25"
 
 #v4.X-rcX + upto SHA
