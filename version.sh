3b68caf00fe8ba01e92957801e82eb0f5fc3617f
Robert Nelson <robertcnelson@gmail.com>
2017-08-15 20:38:38 -0500

    4.9.43-ti-xenomai-r54 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -32,7 +32,7 @@ toolchain="gcc_linaro_gnueabihf_6"
 #Kernel/Build
 KERNEL_REL=4.9
 KERNEL_TAG=${KERNEL_REL}.43
-BUILD=${build_prefix}53.1
+BUILD=${build_prefix}54
 kernel_rt=".40-rt30"
 
 #v4.X-rcX + upto SHA
