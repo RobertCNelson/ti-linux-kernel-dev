3d69f1514a607a3ddba0474b72712b2e3ac7efbe
Robert Nelson <robertcnelson@gmail.com>
2017-10-23 10:44:17 -0500

    merge ti: https://github.com/RobertCNelson/ti-linux-kernel/compare/95604f967394278d8dc0be2289df4079cfe63771...bcd48b4c8be2cd187c696d760ad0709ddee2f9eb
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -34,7 +34,7 @@ KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.91
 kernel_rt=".86-rt99"
 #Kernel Build
-BUILD=${build_prefix}133
+BUILD=${build_prefix}133.1
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
@@ -46,9 +46,9 @@ BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 DISTRO=cross
 DEBARCH=armhf
 
-ti_git_old_release="f71b1daa4723c29e1c9e762957e1a631170dbf78"
-        ti_git_pre="f71b1daa4723c29e1c9e762957e1a631170dbf78"
-       ti_git_post="95604f967394278d8dc0be2289df4079cfe63771"
+ti_git_old_release="95604f967394278d8dc0be2289df4079cfe63771"
+        ti_git_pre="95604f967394278d8dc0be2289df4079cfe63771"
+       ti_git_post="bcd48b4c8be2cd187c696d760ad0709ddee2f9eb"
 #
 
 #https://git.xenomai.org/xenomai-3.git/
