671bd40ad3f42c30643ab389b5a41e45697cbdcd
Robert Nelson <robertcnelson@gmail.com>
2017-11-01 19:32:42 -0500

    pocketbeagle: fix P2.24
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -382,7 +382,7 @@ lts44_backports () {
 	${git} "${DIR}/patches/backports/iio/0016-iio-imu-adis16480-Fix-acceleration-scale-factor-for-.patch"
 	${git} "${DIR}/patches/backports/iio/0017-iio-hid-sensor-trigger-Fix-the-race-with-user-space-.patch"
 
-	backport_tag="v4.9.58"
+	backport_tag="v4.9.59"
 
 	subsystem="fbtft"
 	if [ "x${regenerate}" = "xenable" ] ; then
