307cdc24ebf1b603f3b4e839462178ab809ffff8
Robert Nelson <robertcnelson@gmail.com>
2018-01-29 11:25:55 -0600

    merge ti: https://github.com/RobertCNelson/ti-linux-kernel/compare/fec7a926199d5789d82ee822bd012fd3f18e9054...9d6c262a3b8cb917e35ddb8cbb8cd8377aa0dfee
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -490,6 +490,9 @@ lts44_backports () {
 #v4.4.97
 	${git} "${DIR}/patches/backports/iio/0027-iio-trigger-free-trigger-resource-correctly.patch"
 
+#fix, ti merged in v4.9.x:
+	${git} "${DIR}/patches/backports/iio/0028-mfd-palmas-Assign-the-right-powerhold-mask-for-tps65.patch"
+
 	backport_tag="v4.9.78"
 
 	subsystem="touchscreen"
@@ -527,6 +530,7 @@ lts44_backports () {
 
 	${git} "${DIR}/patches/backports/touchscreen/0002-edt-ft5x06-we-need-these-in-v4.4.x.patch"
 	${git} "${DIR}/patches/backports/touchscreen/0003-ar1021_i2c-invert-swap.patch"
+	${git} "${DIR}/patches/backports/touchscreen/0004-ar1021_i2c.c-introduce-offsets-to-manually-re-calbra.patch"
 
 	subsystem="etnaviv"
 	if [ "x${regenerate}" = "xenable" ] ; then
