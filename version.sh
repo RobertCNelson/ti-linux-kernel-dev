baa272be53e67793bd1fc6b5581eb4ad5f05c46d
Robert Nelson <robertcnelson@gmail.com>
2018-01-29 11:57:01 -0600

    4.4.113-ti-r145 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -34,7 +34,7 @@ KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.113
 kernel_rt=".113-rt128"
 #Kernel Build
-BUILD=${build_prefix}144.1
+BUILD=${build_prefix}145
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
