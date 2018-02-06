8065cbb2c069dd767dda4ba31b4715932a03d194
Robert Nelson <robertcnelson@gmail.com>
2018-02-06 13:50:13 -0600

    config: enable: CONFIG_MMC_SPI
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -34,7 +34,7 @@ KERNEL_REL=4.14
 KERNEL_TAG=${KERNEL_REL}.17
 kernel_rt=".15-rt13"
 #Kernel Build
-BUILD=${build_prefix}31
+BUILD=${build_prefix}31.1
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
@@ -46,7 +46,7 @@ BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 DISTRO=cross
 DEBARCH=armhf
 
-ti_git_old_release="4c5ea68783148c0882d8c8c2907f95283c6b6cd2"
-        ti_git_pre="4c5ea68783148c0882d8c8c2907f95283c6b6cd2"
+ti_git_old_release="4e4fa96300eaf9ef760ca6219d7df873937ec913"
+        ti_git_pre="4e4fa96300eaf9ef760ca6219d7df873937ec913"
        ti_git_post="4e4fa96300eaf9ef760ca6219d7df873937ec913"
 #
