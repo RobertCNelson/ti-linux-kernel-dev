766ab071700816920a52ddaea6be175146792e57
Robert Nelson <robertcnelson@gmail.com>
2018-02-20 12:31:56 -0600

    blue: fixes: dont grab these with pinmux
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -34,7 +34,7 @@ KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.113
 kernel_rt=".113-rt128"
 #Kernel Build
-BUILD=${build_prefix}146.1
+BUILD=${build_prefix}146.2
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
