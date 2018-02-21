3d260b28db9f09bd99e7f7583fc3d669b2ea71ad
Robert Nelson <robertcnelson@gmail.com>
2018-02-21 10:39:25 -0600

    blue: assorted fixes, clock/adc/etc
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -190,7 +190,7 @@ aufs4 () {
 
 		rm -rf ../aufs4-standalone/ || true
 
-		git reset --hard HEAD~5
+		${git_bin} reset --hard HEAD~5
 
 		start_cleanup
 
@@ -311,7 +311,7 @@ wireguard () {
 
 		rm -rf ../WireGuard/ || true
 
-		git reset --hard HEAD^
+		${git_bin} reset --hard HEAD^
 
 		start_cleanup
 
