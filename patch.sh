7439f22825127601e6aa1f67ac540a19eb9db9e7
Robert Nelson <robertcnelson@gmail.com>
2018-12-14 12:01:51 -0600

    merge ti: https://github.com/RobertCNelson/ti-linux-kernel/compare/23c0c00c2f0b857d39bfe98aba7c85391dbd029f...c4fb94f22d680bec41f0f33202de3d1dbccade3b
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -352,9 +352,6 @@ patch_backports (){
 backports () {
 	backport_tag="v4.20-rc6"
 
-	subsystem="am335xdts"
-	patch_backports
-
 	subsystem="brcm80211"
 	#regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
@@ -497,8 +494,7 @@ packaging () {
 	if [ "x${regenerate}" = "xenable" ] ; then
 		cp -v "${DIR}/3rdparty/packaging/Makefile" "${DIR}/KERNEL/scripts/package"
 		cp -v "${DIR}/3rdparty/packaging/builddeb" "${DIR}/KERNEL/scripts/package"
-		#Needed for v4.11.x and less
-		#patch -p1 < "${DIR}/patches/packaging/0002-Revert-deb-pkg-Remove-the-KBUILD_IMAGE-workaround.patch"
+		cp -v "${DIR}/3rdparty/packaging/mkdebian" "${DIR}/KERNEL/scripts/package"
 		${git_bin} commit -a -m 'packaging: sync builddeb changes' -s
 		${git_bin} format-patch -1 -o "${DIR}/patches/packaging"
 		exit 2
--- version.sh
+++ version.sh
@@ -33,10 +33,10 @@ toolchain="gcc_arm_gnueabihf_8"
 
 #Kernel
 KERNEL_REL=4.19
-KERNEL_TAG=${KERNEL_REL}.5
-kernel_rt=".5-rt4"
+KERNEL_TAG=${KERNEL_REL}.9
+kernel_rt=".8-rt6"
 #Kernel Build
-BUILD=${build_prefix}4
+BUILD=${build_prefix}4.1
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
