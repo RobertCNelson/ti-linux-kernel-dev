049643be61b001a7781baaaeb730e5abd4458e4f
Robert Nelson <robertcnelson@gmail.com>
2019-02-11 09:55:39 -0600

    merge ti: https://github.com/RobertCNelson/ti-linux-kernel/compare/efb32b0ef31152f612fcefae8f7511e88da957be...9e24d540e8e0a56cfed99656d589fb32bf8381b5
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -36,7 +36,7 @@ KERNEL_REL=4.19
 KERNEL_TAG=${KERNEL_REL}.15
 kernel_rt=".15-rt12"
 #Kernel Build
-BUILD=${build_prefix}7
+BUILD=${build_prefix}7.1
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
@@ -48,7 +48,7 @@ BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 DISTRO=xross
 DEBARCH=armhf
 
-ti_git_old_release="dfbfb89a200eefeebce4ac17d0c94f0f99ecd467"
-        ti_git_pre="dfbfb89a200eefeebce4ac17d0c94f0f99ecd467"
-       ti_git_post="efb32b0ef31152f612fcefae8f7511e88da957be"
+ti_git_old_release="efb32b0ef31152f612fcefae8f7511e88da957be"
+        ti_git_pre="efb32b0ef31152f612fcefae8f7511e88da957be"
+       ti_git_post="9e24d540e8e0a56cfed99656d589fb32bf8381b5"
 #
