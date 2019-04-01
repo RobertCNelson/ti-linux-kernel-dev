5ee198a32bdb218255dd329aafc6a9f9726c0794
Robert Nelson <robertcnelson@gmail.com>
2019-04-01 16:53:40 -0500

    merge in cypress patchset
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -36,7 +36,7 @@ KERNEL_REL=4.14
 KERNEL_TAG=${KERNEL_REL}.108
 kernel_rt=".106-rt56"
 #Kernel Build
-BUILD=${build_prefix}102
+BUILD=${build_prefix}102.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
