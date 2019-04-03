fa5e0b236a114d793ae6aef13b65f662dfcb3349
Robert Nelson <robertcnelson@gmail.com>
2019-04-03 13:41:12 -0500

    am335x-* add spidev symlink
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -36,7 +36,7 @@ KERNEL_REL=4.9
 KERNEL_TAG=${KERNEL_REL}.147
 kernel_rt=".146-rt125"
 #Kernel Build
-BUILD=${build_prefix}119.1
+BUILD=${build_prefix}119.2
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
