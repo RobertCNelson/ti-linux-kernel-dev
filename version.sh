18c2d3ca293910bb2f09811d2f1e055e3db8ec90
Robert Nelson <robertcnelson@gmail.com>
2019-05-23 15:03:12 -0500

    convert: patches/drivers/ti/eqep/0001-tieqep-forward-port-of-Nathaniel-Lewis-eQEP-driver.patch to external dtbs repo
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -36,7 +36,7 @@ KERNEL_REL=4.14
 KERNEL_TAG=${KERNEL_REL}.108
 kernel_rt=".106-rt56"
 #Kernel Build
-BUILD=${build_prefix}104.9
+BUILD=${build_prefix}104.10
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
