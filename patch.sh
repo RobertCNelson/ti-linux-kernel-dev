231e3addcd201d1dad95b1e407e982d68e52536a
Robert Nelson <robertcnelson@gmail.com>
2019-05-23 18:53:01 -0500

    convert: patches/soc/ti/am335x_olimex_som to external dtbs repo
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -375,6 +375,8 @@ beagleboard_dtbs () {
 
 		device="am335x-abbbi.dtb" ; dtb_makefile_append
 
+		device="am335x-olimex-som.dtb" ; dtb_makefile_append
+
 		${git_bin} add -f arch/arm/boot/dts/
 		${git_bin} add -f include/dt-bindings/
 		${git_bin} commit -a -m "Add BeagleBoard.org DTBS: $bbdtbs" -m "https://github.com/beagleboard/BeagleBoard-DeviceTrees/tree/${bbdtbs}" -s
@@ -683,7 +685,6 @@ drivers () {
 
 soc () {
 	dir 'soc/ti/abbbi'
-	dir 'soc/ti/am335x_olimex_som'
 	dir 'soc/ti/beaglebone_capes'
 	dir 'soc/ti/pocketbeagle'
 	dir 'soc/ti/pruss'
@@ -730,8 +731,6 @@ beaglebone () {
 
 		device="am335x-sancloud-bbe-uboot.dtb" ; dtb_makefile_append
 		device="am335x-sancloud-bbe-uboot-univ.dtb" ; dtb_makefile_append
-
-		device="am335x-olimex-som.dtb" ; dtb_makefile_append
 
 		device="am335x-boneblack-wl1835mod.dtb" ; dtb_makefile_append
 
