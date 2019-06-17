4af71e9e53f6113de311f4b27e0d3c6a695dffbd
Robert Nelson <robertcnelson@gmail.com>
2019-06-17 17:13:27 -0500

    update: brcm80211: backport to v4.17.19
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -36,7 +36,7 @@ KERNEL_REL=4.14
 KERNEL_TAG=${KERNEL_REL}.108
 kernel_rt=".106-rt56"
 #Kernel Build
-BUILD=${build_prefix}107.2
+BUILD=${build_prefix}107.3
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
