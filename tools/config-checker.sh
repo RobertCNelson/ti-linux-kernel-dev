2695ece6e5963c42a6db4fdc059862b7a6f86142
Robert Nelson <robertcnelson@gmail.com>
2019-10-09 17:02:22 -0500

    config: disable CONFIG_ARCH_WM8850
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-checker.sh
+++ tools/config-checker.sh
@@ -97,6 +97,7 @@ config="CONFIG_ARCH_SOCFPGA" ; config_disable
 config="CONFIG_ARCH_SUNXI" ; config_disable
 config="CONFIG_ARCH_TEGRA" ; config_disable
 config="CONFIG_ARCH_VEXPRESS" ; config_disable
+config="CONFIG_ARCH_WM8850" ; config_disable
 
 #
 # Bus support
@@ -140,11 +141,6 @@ config="CONFIG_EMBEDDED" ; config_enable
 # GCOV-based kernel profiling
 #
 config="CONFIG_MODULE_SIG" ; config_disable
-
-#
-# OMAP Legacy Platform Data Board Type
-#
-config="CONFIG_ARCH_WM8850" ; config_disable
 
 #
 # Processor Features
