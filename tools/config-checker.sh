08589bc1771acdce6ee7ae1dc178afc91915b0b3
Robert Nelson <robertcnelson@gmail.com>
2019-10-09 16:27:09 -0500

    config: disable CONFIG_ARCH_MXC
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-checker.sh
+++ tools/config-checker.sh
@@ -73,6 +73,7 @@ config="CONFIG_ARCH_VIRT" ; config_disable
 config="CONFIG_ARCH_BCM" ; config_disable
 config="CONFIG_ARCH_EXYNOS" ; config_disable
 config="CONFIG_ARCH_HIGHBANK" ; config_disable
+config="CONFIG_ARCH_MXC" ; config_disable
 
 #
 # Bus support
@@ -118,7 +119,6 @@ config="CONFIG_EMBEDDED" ; config_enable
 config="CONFIG_MODULE_SIG" ; config_disable
 
 config="CONFIG_ARCH_MVEBU" ; config_disable
-config="CONFIG_ARCH_MXC" ; config_disable
 
 #
 # TI OMAP/AM/DM/DRA Family
