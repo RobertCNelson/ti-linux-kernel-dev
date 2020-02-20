6753f78d80112691de6b49643f996bae2146eabc
Robert Nelson <robertcnelson@gmail.com>
2020-02-20 10:55:59 -0600

    debian: config: resync base with 5.4.19-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-abi="4.19.0-4"
-kernel="4.19.28-2"
+abi="5.4.0-4"
+kernel="5.4.19-1"
 
 debian_site="http://ftp.de.debian.org/debian/pool/main/l/linux"
 incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
