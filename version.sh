f8d1ec27ac8bff421a5f83dd4eaac1f6b2d84aa7
Robert Nelson <robertcnelson@gmail.com>
2020-03-18 10:23:17 -0500

    config: drop cpuidle, this is finally getting merged, lets not support pre device-tree anymore
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -39,7 +39,7 @@ KERNEL_REL=4.14
 KERNEL_TAG=${KERNEL_REL}.108
 kernel_rt=".106-rt56"
 #Kernel Build
-BUILD=${build_prefix}128.2
+BUILD=${build_prefix}128.3
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
