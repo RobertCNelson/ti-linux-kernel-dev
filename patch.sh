8c710a5f76908b02500e8772e0b71b55e20086c9
Robert Nelson <robertcnelson@gmail.com>
2021-05-27 14:14:01 -0500

    kernel v4.14.108 rebase with: wireguard/can_isotp/device-tree/etc AUFS: https://github.com/sfjro/aufs4-standalone/commit/df5c09adc49fd0d431c0f18d190130675c8bffb9 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/554a0cdf48b575076a180b169b9ce9c8fbf7c39a CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/b312dbd5e720833cb69bc65af3670a52dd1e93ca TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=04f3e5411b1cdc59292775e7b8e05a5d35ea4a80
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -467,8 +467,6 @@ beagleboard_dtbs () {
 
 		device="am335x-abbbi.dtb" ; dtb_makefile_append
 
-		device="am335x-boneblack-uboot.dtb" ; dtb_makefile_append
-
 		device="am335x-sancloud-bbe.dtb" ; dtb_makefile_append
 		device="am335x-olimex-som.dtb" ; dtb_makefile_append
 
@@ -486,9 +484,9 @@ beagleboard_dtbs () {
 		device="am335x-boneblack-wireless-roboticscape.dtb" ; dtb_makefile_append
 
 		device="am335x-bone-uboot-univ.dtb" ; dtb_makefile_append
+		device="am335x-boneblack-uboot.dtb" ; dtb_makefile_append
 		device="am335x-boneblack-uboot-univ.dtb" ; dtb_makefile_append
 		device="am335x-bonegreen-wireless-uboot-univ.dtb" ; dtb_makefile_append
-
 		device="am335x-bonegreen-gateway.dtb" ; dtb_makefile_append
 		device="am335x-sancloud-bbe-uboot.dtb" ; dtb_makefile_append
 		device="am335x-sancloud-bbe-uboot-univ.dtb" ; dtb_makefile_append
@@ -665,7 +663,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v4.19.190"
+	backport_tag="v4.19.192"
 
 	subsystem="greybus"
 	#regenerate="enable"
@@ -681,7 +679,7 @@ backports () {
 		${git} "${DIR}/patches/backports/greybus/0002-greybus-drivers-staging-greybus-module.c-no-struct_s.patch"
 	fi
 
-	backport_tag="v4.14.232"
+	backport_tag="v4.14.234"
 
 	subsystem="wlcore"
 	#regenerate="enable"
@@ -696,7 +694,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v4.14.232"
+	backport_tag="v4.14.234"
 
 	subsystem="iio"
 	#regenerate="enable"
@@ -714,7 +712,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.4.120"
+	backport_tag="v5.4.122"
 
 	subsystem="wiznet"
 	#regenerate="enable"
