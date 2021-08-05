8983100580a7711e40c26503639663d61cf3f04e
Robert Nelson <robertcnelson@gmail.com>
2021-08-04 20:44:06 -0500

    merge ti: https://github.com/RobertCNelson/ti-linux-kernel/compare/3314837930ce2cf2bbd51a34f32a7494a6aec8c8...406a8150c46c1e6eb4afac76215eb85731011bd5
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -416,7 +416,7 @@ beagleboard_dtbs () {
 
 		${git_bin} add -f arch/arm/boot/dts/
 		${git_bin} add -f include/dt-bindings/
-		${git_bin} commit -a -m "Add BeagleBoard.org DTBS: $branch" -m "${https_repo}/tree/${branch}" -m "${https_repo}/commit/${git_hash}" -s
+		${git_bin} commit -a -m "Add BeagleBoard.org Device Tree Changes" -m "${https_repo}/tree/${branch}" -m "${https_repo}/commit/${git_hash}" -s
 		${git_bin} format-patch -1 -o ../patches/soc/ti/beagleboard_dtbs/
 		echo "BBDTBS: ${https_repo}/commit/${git_hash}" > ../patches/git/BBDTBS
 
@@ -426,7 +426,7 @@ beagleboard_dtbs () {
 
 		start_cleanup
 
-		${git} "${DIR}/patches/soc/ti/beagleboard_dtbs/0001-Add-BeagleBoard.org-DTBS-$branch.patch"
+		${git} "${DIR}/patches/soc/ti/beagleboard_dtbs/0001-Add-BeagleBoard.org-Device-Tree-Changes.patch"
 
 		wdir="soc/ti/beagleboard_dtbs"
 		number=1
@@ -485,7 +485,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v5.12.11"
+	backport_tag="v5.12.19"
 
 	subsystem="greybus"
 	#regenerate="enable"
@@ -501,7 +501,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.12.11"
+	backport_tag="v5.12.19"
 
 	subsystem="wlcore"
 	#regenerate="enable"
@@ -516,7 +516,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.13-rc6"
+	backport_tag="v5.13.8"
 
 	subsystem="spidev"
 	#regenerate="enable"
@@ -531,7 +531,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.10.44"
+	backport_tag="v5.10.56"
 
 	subsystem="iio"
 	#regenerate="enable"
@@ -600,7 +600,7 @@ soc
 packaging () {
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.10.44"
+		backport_tag="v5.10.56"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
