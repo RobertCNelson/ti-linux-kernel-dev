2abeb72329cb6796195262af51df529d8ba82235
Robert Nelson <robertcnelson@gmail.com>
2021-08-09 19:07:51 -0500

    add ti reference configs
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-abi="5.10.0-6"
-kernel="5.10.28-1"
+abi="5.10.0-8"
+kernel="5.10.46-4"
 
 debian_site="http://ftp.de.debian.org/debian/pool/main/l/linux"
 incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
