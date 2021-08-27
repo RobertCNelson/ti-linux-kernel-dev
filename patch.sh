cb3e4d95903a40cd978d32ddfec7a0649e7b323d
Robert Nelson <robertcnelson@gmail.com>
2021-08-27 13:31:18 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -240,7 +240,7 @@ patch_backports (){
 packaging () {
 	do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.14-rc2"
+		backport_tag="v5.14-rc7"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
