92f0a4f599fcce011b27a950fef28b8e20aa1031
Robert Nelson <robertcnelson@gmail.com>
2021-09-23 16:32:43 -0500

    merge ti: https://github.com/RobertCNelson/ti-linux-kernel/compare/a8b5fd96e0d636c99069d213e7f72f708098febe...8128da9522b896b4908546f5e0b69967381d3d41
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -42,7 +42,7 @@ KERNEL_REL=5.10
 KERNEL_TAG=${KERNEL_REL}.59
 kernel_rt=".59-rt52"
 #Kernel Build
-BUILD=${build_prefix}19.1
+BUILD=${build_prefix}19.2
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
@@ -53,9 +53,9 @@ BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 
 DISTRO=xross
 
-ti_git_old_release="18d726513306c676055b634d1e6bfea6a2ac3699"
-        ti_git_pre="18d726513306c676055b634d1e6bfea6a2ac3699"
-       ti_git_post="a8b5fd96e0d636c99069d213e7f72f708098febe"
+ti_git_old_release="a8b5fd96e0d636c99069d213e7f72f708098febe"
+        ti_git_pre="a8b5fd96e0d636c99069d213e7f72f708098febe"
+       ti_git_post="8128da9522b896b4908546f5e0b69967381d3d41"
 #
 
 #https://source.denx.de/Xenomai/xenomai.git
