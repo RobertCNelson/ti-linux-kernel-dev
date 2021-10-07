2bf28a3536d8dab42b6e149d7e8a9a3651347b56
Robert Nelson <robertcnelson@gmail.com>
2021-10-07 16:08:47 -0500

    kernel v5.10.65 rebase with: device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/271719765b744a4fa7d60de58018e894664bf80d BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/4434c131cd0f38c39c0f3ea8ef32edae657c5753 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=47007d0169c4c6c855a3fc5333c142489a43e89e KSMBD: https://github.com/cifsd-team/ksmbd/commit/0a8e1a7605528fa85901f1837ca3a7a83f3befe4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -42,7 +42,7 @@ KERNEL_REL=5.10
 KERNEL_TAG=${KERNEL_REL}.65
 kernel_rt=".65-rt53"
 #Kernel Build
-BUILD=${build_prefix}23
+BUILD=${build_prefix}23.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
@@ -53,8 +53,8 @@ BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 
 DISTRO=xross
 
-ti_git_old_release="02f617d43504b134d5b730ee0c0caeeb1462fd9e"
-        ti_git_pre="02f617d43504b134d5b730ee0c0caeeb1462fd9e"
+ti_git_old_release="5d12f5b8e60868c69a39c555789fe4824fc774dd"
+        ti_git_pre="5d12f5b8e60868c69a39c555789fe4824fc774dd"
        ti_git_post="5d12f5b8e60868c69a39c555789fe4824fc774dd"
 #
 
