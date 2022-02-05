ddc977ab12537f2e9c781c2aae7c93a9ebbd5b80
Robert Nelson <robertcnelson@gmail.com>
2022-02-05 13:31:16 -0600

    sync debian config
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-abi="5.10.0-9"
-kernel="5.10.70-1"
+#https://packages.debian.org/source/bullseye/linux
+
+abi="5.10.0-10"
+kernel="5.10.84-1"
 
 debian_site="http://ftp.de.debian.org/debian/pool/main/l/linux"
 incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
