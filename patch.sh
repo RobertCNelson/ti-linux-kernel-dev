edceaa1178bf7e35fa33961316982cce6d1e9b7d
Robert Nelson <robertcnelson@gmail.com>
2022-04-21 12:45:29 -0500

    kernel v5.10.109 rebase with: device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/19ba863e92709d8b21915752565a146fd8620eaa BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/88a840b49c9a8458ca850cdbb162ffa324c69c13 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a BCFSERIAL: https://github.com/statropy/bcfserial/commit/aded88429a8a00143596b41f4c1f50d9ae3d4069 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=166fbddc84ba13f17b133073331e015e0f52f0da KSMBD: https://github.com/cifsd-team/ksmbd/commit/cca2e29823daf5d4d91c07c43117339865461f7d
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -623,7 +623,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.10.111"
+	backport_tag="v5.10.112"
 
 	subsystem="iio"
 	#regenerate="enable"
@@ -641,7 +641,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.15.34"
+	backport_tag="v5.15.35"
 
 	subsystem="pinmux"
 	#regenerate="enable"
@@ -853,7 +853,7 @@ fixes
 packaging () {
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.17.3"
+		backport_tag="v5.17.4"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
