5e7f5119fb390f8fe8f51a6f8bdaa2b3c5252df8
Robert Nelson <robertcnelson@gmail.com>
2022-07-20 14:28:16 -0500

    merge ti: https://github.com/RobertCNelson/ti-linux-kernel/compare/016e97907aa4a3194540d461baf33c2d7439db75...1e555a0d9d4e2667414875c20d041d0621028c2c AUFS: https://github.com/sfjro/aufs5-standalone/commit/a86ab233533efb15ab56c4c5747e685227f64414 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/dfce86562e5d71ac11d67d9734181de28fdcbc38 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a BCFSERIAL: https://github.com/statropy/bcfserial/commit/aded88429a8a00143596b41f4c1f50d9ae3d4069 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=68a8f2f89cc58989bf4c1ade4757b2d5b680127e KSMBD: https://github.com/cifsd-team/ksmbd/commit/ad3bced04a30ea2fb0070330489d9452e2d6c69c
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -625,7 +625,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.10.126"
+	backport_tag="v5.10.131"
 
 	subsystem="iio"
 	#regenerate="enable"
@@ -643,7 +643,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.15.50"
+	backport_tag="v5.15.55"
 
 	subsystem="pinmux"
 	#regenerate="enable"
@@ -856,7 +856,7 @@ fixes
 packaging () {
 	do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.17.15"
+		backport_tag="v5.18.12"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
