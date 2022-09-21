f751402d94ec0a022d3799439246cdd79b753016
Robert Nelson <robertcnelson@gmail.com>
2022-09-21 11:49:56 -0500

    config: enable CONFIG_NTFS_FS=m, CONFIG_NTFS_RW=y
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -45,7 +45,7 @@ KERNEL_REL=5.10
 KERNEL_TAG=${KERNEL_REL}.131
 kernel_rt=".131-rt72"
 #Kernel Build
-BUILD=${build_prefix}49
+BUILD=${build_prefix}49.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
@@ -56,6 +56,6 @@ BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 
 DISTRO=xross
 
-ti_git_old_release="1e555a0d9d4e2667414875c20d041d0621028c2c"
+ti_git_old_release="0bf568276c5756fe4537bd6bd3277768d6ad2969"
 ti_git_new_release="0bf568276c5756fe4537bd6bd3277768d6ad2969"
 #
