bbeed8a4f4091a58907817b7a211fa9824ae7c64
Robert Nelson <robertcnelson@gmail.com>
2022-10-07 16:31:38 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -4,5 +4,5 @@ DIR=$PWD
 
 . "${DIR}/version.sh"
 
-echo "syncing with:https://git.beagleboard.org/RobertCNelson/ti-linux-kernel-dev.git ${branch_prefix}${bborg_branch}${branch_postfix}"
-git pull --no-edit https://git.beagleboard.org/RobertCNelson/ti-linux-kernel-dev.git ${branch_prefix}${bborg_branch}${branch_postfix}
+echo "syncing with:https://git.beagleboard.org/RobertCNelson/ti-linux-kernel-dev.git ${branch_prefix}${KERNEL_REL}${branch_postfix}"
+git pull --no-edit https://git.beagleboard.org/RobertCNelson/ti-linux-kernel-dev.git ${branch_prefix}${KERNEL_REL}${branch_postfix}
