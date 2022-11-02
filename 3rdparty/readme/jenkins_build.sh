7bb536fda8080669ea26eb56097a68568a8484ee
Robert Nelson <robertcnelson@gmail.com>
2022-11-02 13:58:10 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 3rdparty/readme/jenkins_build.sh
+++ 3rdparty/readme/jenkins_build.sh
@@ -3,10 +3,13 @@
 #git clone -b 5.10 https://github.com/beagleboard/linux --depth=10
 #cd ./linux
 
+CORES=$(getconf _NPROCESSORS_ONLN)
+
 export CC=/usr/bin/arm-linux-gnueabihf-
 
 make ARCH=arm CROSS_COMPILE=${CC} clean
 make ARCH=arm CROSS_COMPILE=${CC} bb.org_defconfig
 
-echo "make -j12 ARCH=arm KBUILD_DEBARCH=armhf CROSS_COMPILE=${CC} bindeb-pkg"
-make -j12 ARCH=arm KBUILD_DEBARCH=armhf KDEB_PKGVERSION=1xross CROSS_COMPILE=${CC} bindeb-pkg
+echo "make -j${CORES} ARCH=arm KBUILD_DEBARCH=armhf CROSS_COMPILE=${CC} bindeb-pkg"
+make -j${CORES} ARCH=arm KBUILD_DEBARCH=armhf KDEB_PKGVERSION=1xross CROSS_COMPILE=${CC} bindeb-pkg
+mv ../*.deb ./
