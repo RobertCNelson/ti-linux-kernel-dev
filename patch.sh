536e02465dde9744999d16b49619a9a06ca907dc
Robert Nelson <robertcnelson@gmail.com>
2022-11-02 16:47:29 -0500

    cleanup: move iio patches, after iio backport
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -554,6 +554,9 @@ backports () {
 		exit 2
 	else
 		patch_backports
+		${git} "${DIR}/patches/backports/${subsystem}/0002-iio-adc-ti-adc128s052-Fix-number-of-channels-when-de.patch"
+		${git} "${DIR}/patches/backports/${subsystem}/0003-dt-bindings-iio-adc-ti-adc128s052-Add-adc08c-and-adc.patch"
+		${git} "${DIR}/patches/backports/${subsystem}/0004-iio-adc-ti-adc128s052-Add-lower-resolution-devices-s.patch"
 	fi
 
 	backport_tag="v5.15.76"
