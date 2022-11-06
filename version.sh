814ad9fb42d0b4a1a05b6bb9b3740ab24c233ea0
Robert Nelson <robertcnelson@gmail.com>
2022-11-05 21:13:20 -0500

    merge ti: https://github.com/RobertCNelson/ti-linux-kernel/compare/018bd5143155c70167c94d3c6c369b9651e00fd9...8b51d20b6e6e1b9277b59b7aaed8a97eff43097f AUFS: https://github.com/sfjro/aufs-standalone/commit/2c2dfeb062b323e07fd1e3a0d1c3eb816da25a8b BBDTBS: https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/db7deb36ee68fbcb20f8035871f7c7bb91f180cb RT: patch-5.10.145-rt74.patch.xz WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a BCFSERIAL: https://github.com/statropy/bcfserial/commit/aded88429a8a00143596b41f4c1f50d9ae3d4069 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=c04fd9bcfe49ee1071882cc543ed518f65070204 KSMBD: https://github.com/cifsd-team/ksmbd/commit/c5864a9e60afd9bd8b66f524f9985df778480c66
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -45,7 +45,7 @@ KERNEL_REL=5.10
 KERNEL_TAG=${KERNEL_REL}.145
 kernel_rt=".145-rt74"
 #Kernel Build
-BUILD=${build_prefix}71
+BUILD=${build_prefix}71.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
@@ -56,8 +56,11 @@ BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 
 DISTRO=xross
 
-ti_git_old_release="5e63ae91b2138efad3db617a0ecb5ff76c591131"
-ti_git_new_release="018bd5143155c70167c94d3c6c369b9651e00fd9"
+ti_git_old_release="018bd5143155c70167c94d3c6c369b9651e00fd9"
+ti_git_new_release="8b51d20b6e6e1b9277b59b7aaed8a97eff43097f"
+
+#https://git.ti.com/gitweb?p=ti-linux-kernel/ti-linux-kernel.git;a=tag;h=refs/tags/08.05.00.003
+#https://git.ti.com/gitweb?p=ti-linux-kernel/ti-linux-kernel.git;a=shortlog;h=8b51d20b6e6e1b9277b59b7aaed8a97eff43097f
 
 #https://git.ti.com/gitweb?p=ti-linux-kernel/ti-linux-kernel.git;a=tag;h=refs/tags/08.05.00.001
 #https://git.ti.com/gitweb?p=ti-linux-kernel/ti-linux-kernel.git;a=shortlog;h=018bd5143155c70167c94d3c6c369b9651e00fd9
