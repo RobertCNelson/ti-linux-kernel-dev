19f6033d4e71e77cb7096a207737777c4876372a
Robert Nelson <robertcnelson@gmail.com>
2022-12-07 10:02:10 -0600

    reset: 5.10.145-ti-arm64-r78
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -538,7 +538,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v5.10.153"
+	backport_tag="v5.10.155"
 
 	subsystem="iio"
 	#regenerate="enable"
@@ -575,7 +575,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.18.19"
+	backport_tag="v6.0.10"
 
 	subsystem="it66121"
 	#regenerate="enable"
@@ -589,6 +589,7 @@ backports () {
 	else
 		patch_backports
 		${git} "${DIR}/patches/backports/${subsystem}/0002-wire-up-it66121.patch"
+		${git} "${DIR}/patches/backports/${subsystem}/0003-ite-it66121.c-fixes.patch"
 	fi
 
 	backport_tag="v5.11.22"
@@ -654,8 +655,11 @@ drivers () {
 	dir 'drm-bridge'
 
 	dir 'tiam62x'
-	#dir 'android'
+	dir 'android'
 	dir 'ti-edgeai'
+	dir 'lincolntech'
+	dir 'tidss_dispc'
+	dir 'tidss'
 }
 
 ###
