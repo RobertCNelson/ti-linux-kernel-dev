bdf8ca0de6676b6ec2f416c962eb367f4312e5a7
Robert Nelson <robertcnelson@gmail.com>
2023-02-03 19:29:55 -0600

    merge ti: https://github.com/RobertCNelson/ti-linux-kernel/compare/06bd1d157b9b43163c1a2184e4a0c92f98656dd0...2927372e2c7cc3782c07b1896757962bc346d4c5 AUFS: https://github.com/sfjro/aufs-standalone/commit/72075d1d0bd59c94529348883e810410318674e5 BBDTBS: https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/fab7ca900247d3e9041cf526ea2c1fe284309987 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://git.beagleboard.org/beagleconnect/linux/wpanusb/-/commit/6aa9bf65b9d88a2c9a111e7b4aed03de2be9413d BCFSERIAL: https://git.beagleboard.org/beagleconnect/linux/bcfserial/-/commit/db467023bd136c97c2e13c3a8b9e41dbdfafbc66 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=fe05cc94e070c71486047002fc2edf68f5f2a07a KSMBD: https://github.com/cifsd-team/ksmbd/commit/9912f7960ee50fdb42daa28d198dc214fb92983f
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -45,7 +45,7 @@ KERNEL_REL=5.10
 KERNEL_TAG=${KERNEL_REL}.162
 kernel_rt=".162-rt79"
 #Kernel Build
-BUILD=${build_prefix}55.4
+BUILD=${build_prefix}55.5
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
@@ -56,9 +56,12 @@ BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 
 DISTRO=xross
 
-ti_git_old_release="58c3afb61814c1271156d6ace8d0550475268d3f"
-ti_git_new_release="06bd1d157b9b43163c1a2184e4a0c92f98656dd0"
+ti_git_old_release="06bd1d157b9b43163c1a2184e4a0c92f98656dd0"
+ti_git_new_release="2927372e2c7cc3782c07b1896757962bc346d4c5"
 
+#https://git.ti.com/gitweb?p=ti-linux-kernel/ti-linux-kernel.git;a=tag;h=refs/tags/08.06.00.004
+#https://git.ti.com/gitweb?p=ti-linux-kernel/ti-linux-kernel.git;a=commit;h=2927372e2c7cc3782c07b1896757962bc346d4c5
+#
 #https://git.ti.com/gitweb?p=ti-linux-kernel/ti-linux-kernel.git;a=tag;h=refs/tags/08.06.00.003
 #https://git.ti.com/gitweb?p=ti-linux-kernel/ti-linux-kernel.git;a=commit;h=06bd1d157b9b43163c1a2184e4a0c92f98656dd0
 #
