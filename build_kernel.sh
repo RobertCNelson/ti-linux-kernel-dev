5c3a86b7986ac2ad3809c1c98e5dcd2303633511
Robert Nelson <robertcnelson@gmail.com>
2023-05-12 17:20:41 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -45,19 +45,22 @@ copy_defconfig () {
 	cd "${DIR}/KERNEL" || exit
 	make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" distclean
 
-	#./ti_config_fragments/defconfig_builder.sh -l
-	./ti_config_fragments/defconfig_builder.sh -t ti_sdk_arm64_release
-	./ti_config_fragments/defconfig_builder.sh -t ti_sdk_arm64_rt_release
+	if [ -f ./ti_config_fragments/defconfig_builder.sh ] ; then
+		#./ti_config_fragments/defconfig_builder.sh -l
+		./ti_config_fragments/defconfig_builder.sh -t ti_sdk_arm64_release
+		./ti_config_fragments/defconfig_builder.sh -t ti_sdk_arm64_rt_release
 
-	make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" distclean
-	make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" ti_sdk_arm64_release_defconfig
-	cp -v .config "${DIR}/patches/ti_sdk_arm64_release_defconfig"
+		make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" distclean
+		make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" ti_sdk_arm64_release_defconfig
+		cp -v .config "${DIR}/patches/ti_sdk_arm64_release_defconfig"
 
-	make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" distclean
-	make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" ti_sdk_arm64_rt_release_defconfig
-	cp -v .config "${DIR}/patches/ti_sdk_arm64_rt_release_defconfig"
+		make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" distclean
+		make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" ti_sdk_arm64_rt_release_defconfig
+		cp -v .config "${DIR}/patches/ti_sdk_arm64_rt_release_defconfig"
 
-	make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" distclean
+		make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" distclean
+	fi
+
 	if [ ! -f "${DIR}/.yakbuild" ] ; then
 		make ARCH=${KERNEL_ARCH} CROSS_COMPILE="${CC}" "${config}"
 		cp -v .config "${DIR}/patches/ref_${config}"
--- patch.sh
+++ patch.sh
@@ -656,11 +656,9 @@ packaging () {
 }
 
 readme () {
-	#regenerate="enable"
+	regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
 		cp -v "${DIR}/3rdparty/readme/README.md" "${DIR}/KERNEL/README.md"
-		cp -v "${DIR}/3rdparty/readme/jenkins_build.sh" "${DIR}/KERNEL/jenkins_build.sh"
-		cp -v "${DIR}/3rdparty/readme/Jenkinsfile" "${DIR}/KERNEL/Jenkinsfile"
 		cp -v "${DIR}/3rdparty/readme/.gitlab-ci.yml" "${DIR}/KERNEL/.gitlab-ci.yml"
 
 		mkdir -p "${DIR}/KERNEL/.github/ISSUE_TEMPLATE/"
@@ -668,14 +666,12 @@ readme () {
 		cp -v "${DIR}/3rdparty/readme/FUNDING.yml" "${DIR}/KERNEL/.github/"
 
 		git add -f README.md
-		git add -f jenkins_build.sh
-		git add -f Jenkinsfile
 		git add -f .gitlab-ci.yml
 
 		git add -f .github/ISSUE_TEMPLATE/bug_report.md
 		git add -f .github/FUNDING.yml
 
-		git commit -a -m 'enable: Jenkins/gitlab-ci' -s
+		git commit -a -m 'enable: gitlab-ci' -s
 		git format-patch -1 -o "${DIR}/patches/readme"
 		exit 2
 	else
