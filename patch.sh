63e95f0142a6f03fd5e40b4be33d6f3c5c0ebf9e
Robert Nelson <robertcnelson@gmail.com>
2023-05-12 17:24:02 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -656,7 +656,7 @@ packaging () {
 }
 
 readme () {
-	regenerate="enable"
+	#regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
 		cp -v "${DIR}/3rdparty/readme/README.md" "${DIR}/KERNEL/README.md"
 		cp -v "${DIR}/3rdparty/readme/.gitlab-ci.yml" "${DIR}/KERNEL/.gitlab-ci.yml"
