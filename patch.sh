eb3f96bc7a97559bb542e896a37189790679bdf3
Robert Nelson <robertcnelson@gmail.com>
2023-06-12 17:04:42 -0500

    Merge TI Branch; https://github.com/RobertCNelson/ti-linux-kernel/compare/30bcc25e7cb1b25667db88d61828728f0f0b5f4b...7124dee9779a40c52c52331c3ba6ccc84f357380 TI SDK: 09.00.00.002 BBDTBS: https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/9da3364474b349d7e5ccc14de93f2e881f4a81e4 AUFS: https://github.com/sfjro/aufs-standalone/commit/80bbd686e8be627d87bfa0ede768c2a52843647f WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=9dc0800cdd7aeeae273b983973a1749f73008291 KSMBD: https://github.com/cifsd-team/ksmbd/commit/11aa06fbc7307b3ff68ebbdb96d42484d9b5ae6a TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://git.beagleboard.org/beagleconnect/linux/wpanusb/-/commit/6aa9bf65b9d88a2c9a111e7b4aed03de2be9413d BCFSERIAL: https://git.beagleboard.org/beagleconnect/linux/bcfserial/-/commit/db467023bd136c97c2e13c3a8b9e41dbdfafbc66
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -553,7 +553,7 @@ patch_backports () {
 }
 
 backports () {
-	backport_tag="v5.10.180"
+	backport_tag="v5.10.183"
 
 	subsystem="uio"
 	#regenerate="enable"
@@ -569,7 +569,7 @@ backports () {
 		dir 'drivers/ti/uio'
 	fi
 
-	backport_tag="v6.1.29"
+	backport_tag="v6.1.33"
 
 	subsystem="iio"
 	#regenerate="enable"
@@ -585,7 +585,25 @@ backports () {
 		exit 2
 	else
 		patch_backports
+		${git} "${DIR}/patches/backports/${subsystem}/0003-dt-bindings-iio-adc-ti-adc128s052-Add-adc08c-and-adc.patch"
+		${git} "${DIR}/patches/backports/${subsystem}/0004-iio-adc-ti-adc128s052-Add-lower-resolution-devices-s.patch"
 	fi
+
+	backport_tag="v6.1.33"
+
+	subsystem="it66121"
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		pre_backports
+
+		cp -v ~/linux-src/drivers/gpu/drm/bridge/ite-it66121.c ./drivers/gpu/drm/bridge/
+
+		post_backports
+		exit 2
+	#else
+		patch_backports
+	fi
+	${git} "${DIR}/patches/backports/${subsystem}/0003-ite-it66121.c-fixes.patch"
 }
 
 brcmfmac () {
@@ -823,7 +841,7 @@ drivers
 packaging () {
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v6.1.27"
+		backport_tag="v6.1.33"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
@@ -838,7 +856,6 @@ packaging () {
 			patch_backports
 		fi
 	fi
-
 	${git} "${DIR}/patches/backports/bindeb-pkg/0002-builddeb-Install-our-dtbs-under-boot-dtbs-version.patch"
 }
 
