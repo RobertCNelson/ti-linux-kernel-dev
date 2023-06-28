9412a1f44f9dabfe54673031169d2aee6b1a46f7
Robert Nelson <robertcnelson@gmail.com>
2023-06-27 19:13:25 -0500

    kernel v5.4.106-ti-rt-r41.2 rebase external git projects BBDTBS: https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/8713eb53254bdfb9fa6c157e08ef989fd49f425f
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -20,6 +20,7 @@ toolchain="gcc_8_arm"
 #toolchain="gcc_10_arm"
 #toolchain="gcc_11_arm"
 #toolchain="gcc_12_arm"
+#toolchain="gcc_13_arm"
 #arm64
 #KERNEL_ARCH=arm64
 #DEBARCH=arm64
@@ -30,6 +31,7 @@ toolchain="gcc_8_arm"
 #toolchain="gcc_10_aarch64"
 #toolchain="gcc_11_aarch64"
 #toolchain="gcc_12_aarch64"
+#toolchain="gcc_13_aarch64"
 #riscv64
 #KERNEL_ARCH=riscv
 #DEBARCH=riscv64
@@ -39,15 +41,16 @@ toolchain="gcc_8_arm"
 #toolchain="gcc_10_riscv64"
 #toolchain="gcc_11_riscv64"
 #toolchain="gcc_12_riscv64"
+#toolchain="gcc_13_riscv64"
 
 #Kernel
 KERNEL_REL=5.4
 KERNEL_TAG=${KERNEL_REL}.106
 kernel_rt=".106-rt54"
 #Kernel Build
-BUILD=${build_prefix}41
+BUILD=${build_prefix}41.2
 
-#v5.X-rcX + upto SHA
+#v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
 #KERNEL_SHA=""
 
