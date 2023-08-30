26f4fda625de7fe0e0b6887cf90ea244986d5100
Robert Nelson <robertcnelson@gmail.com>
2023-08-30 11:46:37 -0500

    debian: 6.1.38-4
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -2,8 +2,8 @@
 
 #https://packages.debian.org/source/bookworm/linux
 
-abi="6.1.0-9"
-kernel="6.1.27-1"
+abi="6.1.0-11"
+kernel="6.1.38-4"
 
 debian_site="http://ftp.de.debian.org/debian/pool/main/l/linux"
 incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
