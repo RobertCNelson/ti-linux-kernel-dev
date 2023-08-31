1332bdfecfa78a4cce4d262c9909ab4d6f2569c1
Robert Nelson <robertcnelson@gmail.com>
2023-08-31 15:51:11 -0500

    kernel v5.10.168-ti-rt-r69.1 rebase external git projects and rt: v5.10.168-rt83 TI SDK: 08.06.00.007 BBDTBS: https://git.beagleboard.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/78e0d983987866149da4b77d926ece5139feb314 RT: patch-5.10.168-rt83.patch.xz AUFS: https://github.com/sfjro/aufs-standalone/commit/0c55dd9dcf13d5de98218aca4b1a6ad1181acb33 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=ae1421fc746f456c6124d6265413c28460a85f31 KSMBD: https://github.com/cifsd-team/ksmbd/commit/bf249dcef4d5a20eabf9656154419a0042769b90 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://git.beagleboard.org/beagleconnect/linux/wpanusb/-/commit/6aa9bf65b9d88a2c9a111e7b4aed03de2be9413d BCFSERIAL: https://git.beagleboard.org/beagleconnect/linux/bcfserial/-/commit/db467023bd136c97c2e13c3a8b9e41dbdfafbc66
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -579,7 +579,7 @@ backports () {
 
 	dir 'backports/spidev'
 
-	backport_tag="v5.10.188"
+	backport_tag="v5.10.193"
 
 	subsystem="spi"
 	#regenerate="enable"
@@ -594,7 +594,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.10.188"
+	backport_tag="v5.10.193"
 
 	subsystem="iio"
 	#regenerate="enable"
