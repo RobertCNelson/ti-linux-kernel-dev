5bb4ad98dea6338f7fa8d8e01293f2addf95eb54
Robert Nelson <robertcnelson@gmail.com>
2023-08-31 20:26:25 -0500

    debian: 5.10.191-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-abi="5.10.0-14"
-kernel="5.10.113-1"
+abi="5.10.0-25"
+kernel="5.10.191-1"
 
 debian_site="http://ftp.de.debian.org/debian/pool/main/l/linux"
 incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
