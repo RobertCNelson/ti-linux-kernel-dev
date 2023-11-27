2f12a371cb2ae69162a2820bfe2e229de75fd2c7
Robert Nelson <robertcnelson@gmail.com>
2023-11-27 16:33:51 -0600

    drop: BCFSERIAL
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- repo_maintenance/push-kernel-n-test.sh
+++ repo_maintenance/push-kernel-n-test.sh
@@ -29,10 +29,6 @@ cat_files () {
 	if [ -f ./patches/external/git/WPANUSB ] ; then
 		cat ./patches/external/git/WPANUSB >> ${wfile}
 	fi
-
-	if [ -f ./patches/external/git/BCFSERIAL ] ; then
-		cat ./patches/external/git/BCFSERIAL >> ${wfile}
-	fi
 }
 
 DIR=$PWD
