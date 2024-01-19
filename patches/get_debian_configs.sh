157aa614ef295a020c6d7ea81361256b24b5df12
Robert Nelson <robertcnelson@gmail.com>
2024-01-19 14:40:09 -0600

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+#
 #https://packages.debian.org/source/bullseye/linux
-
 #
-abi="5.10.0-26"
-kernel="5.10.197-1"
+abi="5.10.0-27"
+kernel="5.10.205-2"
 #
 
+mirror_site="http://192.168.1.12/debian/pool/main/l/linux"
 debian_site="http://ftp.us.debian.org/debian/pool/main/l/linux"
 incoming_site="http://incoming.debian.org/debian-buildd/pool/main/l/linux"
 
 dl_deb () {
-	wget -c --directory-prefix=./dl/ ${debian_site}/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb
+	wget -c --directory-prefix=./dl/ ${mirror_site}/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb
+
+	if [ ! -f ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ] ; then
+		wget -c --directory-prefix=./dl/ ${debian_site}/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb
+	fi
 
 	if [ ! -f ./dl/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb ] ; then
 		wget -c --directory-prefix=./dl/ ${incoming_site}/linux-image-${abi}-${image}${unsigned}_${kernel}_${dpkg_arch}.deb
