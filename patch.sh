db0baefc52c25a3a328457b815570dddb273bee3
Robert Nelson <robertcnelson@gmail.com>
2024-01-29 11:37:56 -0600

    Merge TI Branch; https://github.com/RobertCNelson/ti-linux-kernel/compare/82d2b827840254248a0444a9c50ab3dc395876cc...78d2216bc679e98b7edb07ffabddb6d0337b5fcb TI SDK: 09.02.00.002 BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/a2fc2b7a96f3b8053d4140f247e897cfdb135ebf WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=37dcea0e6e5effb4228fe385e906edba3cbee389 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://openbeagle.org/beagleconnect/linux/wpanusb/-/commit/6aa9bf65b9d88a2c9a111e7b4aed03de2be9413d
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -473,7 +473,7 @@ patch_backports () {
 }
 
 backports () {
-	backport_tag="v5.10.207"
+	backport_tag="v5.10.209"
 
 	subsystem="uio"
 	#regenerate="enable"
@@ -489,7 +489,7 @@ backports () {
 		dir 'drivers/ti/uio'
 	fi
 
-	backport_tag="v6.1.72"
+	backport_tag="v6.1.75"
 
 	subsystem="iio"
 	#regenerate="enable"
@@ -558,7 +558,7 @@ packaging () {
 	echo "Update: package scripts"
 	#do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v6.1.72"
+		backport_tag="v6.1.75"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
