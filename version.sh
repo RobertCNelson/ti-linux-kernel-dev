a001e27da13bab270c29bba765618c61117c2862
Robert Nelson <robertcnelson@gmail.com>
2024-11-13 08:34:06 -0600

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -47,9 +47,8 @@ toolchain="gcc_13_aarch64"
 #toolchain="gcc_14_riscv64"
 
 #Kernel
-#linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/linux.git"
+linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/linux.git"
 #linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
-linux_repo="https://github.com/beagleboard/mirror-ti-linux-kernel.git"
 linux_stable_repo="https://github.com/beagleboard/mirror-ti-linux-kernel.git"
 #
 KERNEL_REL=6.6
