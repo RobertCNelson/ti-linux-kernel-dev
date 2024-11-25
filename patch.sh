e3dce276c3512058c9ec08f8c201f80a3a7aa6c8
Robert Nelson <robertcnelson@gmail.com>
2024-11-25 11:47:02 -0600

    add: k3-am625-pocketbeagle2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -298,6 +298,7 @@ k3_makefile_patch_cleanup_overlays () {
 	echo "# Enable support for device-tree overlays" >> arch/arm64/boot/dts/ti/Makefile
 	cat arch/arm64/boot/dts/ti/Makefile.dtc >> arch/arm64/boot/dts/ti/Makefile
 	rm arch/arm64/boot/dts/ti/Makefile.dtc
+	echo "DTC_FLAGS_k3-am625-pocketbeagle2 += -@" >> arch/arm64/boot/dts/ti/Makefile
 	echo "DTC_FLAGS_k3-am67a-beagley-ai += -@" >> arch/arm64/boot/dts/ti/Makefile
 	echo "DTC_FLAGS_k3-j721e-beagleboneai64 += -@" >> arch/arm64/boot/dts/ti/Makefile
 }
@@ -397,6 +398,8 @@ beagleboard_dtbs () {
 		device="k3-am625-beaglemod-rs485-2" ; k3_dtbo_makefile_append
 		device="k3-am625-beaglemod-rtc" ; k3_dtbo_makefile_append
 		device="k3-am625-beaglemod-wl1835" ; k3_dtbo_makefile_append
+
+		device="k3-am625-pocketbeagle2" ; k3_dtbo_makefile_append
 
 		#ls src/arm64/overlays/ | grep beagley
 
