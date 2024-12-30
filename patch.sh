d77ae675b3d7f653f5b10f22cc572c1147e7ea54
Robert Nelson <robertcnelson@gmail.com>
2024-12-30 12:45:50 -0600

    merge: mspm0l110x programming
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -269,7 +269,7 @@ beagleboard_dtbs () {
 	branch="v6.12.x-Beagle"
 	https_repo="https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees.git"
 	work_dir="BeagleBoard-DeviceTrees"
-	#regenerate="enable"
+	regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
 		cd ../
 		if [ -d ./${work_dir} ] ; then
