b85209cd4e901f7aa25b24f859349eba629c0825
Robert Nelson <robertcnelson@gmail.com>
2025-01-23 16:58:28 -0600

    kernel v6.1.83-ti-rt-r35.1 rebase external git projects and rt: v6.1.83-rt28 TI SDK: 09.02.00.010 BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/4af7101cac84187b6abcc4f123692835d9c2420a RT: patch-6.1.83-rt28.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=f67f40d009199ee75c906f1ac898b38be981a7f7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -95,6 +95,29 @@ config="CONFIG_REMOTEPROC_CDEV" ; config_enable
 config="CONFIG_WKUP_M3_RPROC" ; config_enable
 config="CONFIG_PRU_REMOTEPROC" ; config_module
 
+#Docker.io
+./scripts/config --enable CONFIG_NETFILTER_XT_MATCH_IPVS
+./scripts/config --enable CONFIG_CGROUP_BPF
+./scripts/config --enable CONFIG_BLK_DEV_THROTTLING
+./scripts/config --enable CONFIG_NET_CLS_CGROUP
+./scripts/config --enable CONFIG_CGROUP_NET_PRIO
+./scripts/config --enable CONFIG_IP_NF_TARGET_REDIRECT
+./scripts/config --enable CONFIG_IP_VS
+./scripts/config --enable CONFIG_IP_VS_NFCT
+./scripts/config --enable CONFIG_IP_VS_PROTO_TCP
+./scripts/config --enable CONFIG_IP_VS_PROTO_UDP
+./scripts/config --enable CONFIG_IP_VS_RR
+./scripts/config --enable CONFIG_SECURITY_SELINUX
+./scripts/config --enable CONFIG_SECURITY_APPARMOR
+./scripts/config --enable CONFIG_VXLAN
+./scripts/config --enable CONFIG_IPVLAN
+./scripts/config --enable CONFIG_DUMMY
+./scripts/config --enable CONFIG_NF_NAT_FTP
+./scripts/config --enable CONFIG_NF_CONNTRACK_FTP
+./scripts/config --enable CONFIG_NF_NAT_TFTP
+./scripts/config --enable CONFIG_NF_CONNTRACK_TFTP
+./scripts/config --enable CONFIG_DM_THIN_PROVISIONING
+
 #abi="5.13.0-trunk"
 #kernel="5.13.9-1~exp2"
 config="CONFIG_BPF_UNPRIV_DEFAULT_OFF" ; config_enable
@@ -221,7 +244,39 @@ config="CONFIG_USB_TI_CPPI41_DMA" ; config_disable
 ./scripts/config --enable CONFIG_MODULE_COMPRESS_XZ
 ./scripts/config --enable CONFIG_GPIO_AGGREGATOR
 
+#new in v6.12.x
+./scripts/config --module CONFIG_RPMB
+./scripts/config --module CONFIG_ADXL380_SPI
+./scripts/config --module CONFIG_ADXL380_I2C
+./scripts/config --module CONFIG_AD4000
+./scripts/config --module CONFIG_AD4695
+./scripts/config --module CONFIG_PAC1921
+./scripts/config --module CONFIG_LTC2664
+./scripts/config --module CONFIG_ENS210
+./scripts/config --module CONFIG_BH1745
+./scripts/config --module CONFIG_SDP500
+./scripts/config --module CONFIG_HX9023S
+./scripts/config --module CONFIG_AW96103
+
+#debian 6.12~rc6-1~exp1
+./scripts/config --enable CONFIG_ZONE_DEVICE
+./scripts/config --module CONFIG_IP_VS_TWOS
+./scripts/config --module CONFIG_VIDEO_OV5648
+./scripts/config --enable CONFIG_DRM_DISPLAY_DP_AUX_CHARDEV
+./scripts/config --module CONFIG_TI_PRUSS
+
+#debian 6.12.6-1
+./scripts/config --enable CONFIG_ZRAM_BACKEND_LZ4
+./scripts/config --enable CONFIG_ZRAM_BACKEND_LZ4HC
+./scripts/config --enable CONFIG_ZRAM_BACKEND_ZSTD
+./scripts/config --enable CONFIG_ZRAM_BACKEND_DEFLATE
+./scripts/config --enable CONFIG_ZRAM_DEF_COMP_LZ4
+./scripts/config --set-str CONFIG_ZRAM_DEF_COMP "lz4"
+
 #configure CONFIG_EXTRA_FIRMWARE
 ./scripts/config --set-str CONFIG_EXTRA_FIRMWARE "regulatory.db regulatory.db.p7s am335x-pm-firmware.elf am335x-bone-scale-data.bin am335x-evm-scale-data.bin am43x-evm-scale-data.bin"
+./scripts/config --enable CONFIG_FW_LOADER_COMPRESS
+./scripts/config --enable CONFIG_FW_LOADER_COMPRESS_XZ
+./scripts/config --enable CONFIG_FW_LOADER_COMPRESS_ZSTD
 
 cd ${DIR}/
