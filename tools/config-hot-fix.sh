b2d02a9537e0f13b8e4b38fa09757c8b95caeafe
Robert Nelson <robertcnelson@gmail.com>
2025-01-23 13:57:06 -0600

    config: add CONFIG_FW_LOADER_COMPRESS_ZSTD
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -284,7 +284,7 @@ config="CONFIG_USB_TI_CPPI41_DMA" ; config_disable
 ./scripts/config --set-str CONFIG_EXTRA_FIRMWARE "regulatory.db regulatory.db.p7s am335x-pm-firmware.elf am335x-bone-scale-data.bin am335x-evm-scale-data.bin am43x-evm-scale-data.bin"
 ./scripts/config --enable CONFIG_FW_LOADER_COMPRESS
 ./scripts/config --enable CONFIG_FW_LOADER_COMPRESS_XZ
-./scripts/config --disable CONFIG_FW_LOADER_COMPRESS_ZSTD
+./scripts/config --enable CONFIG_FW_LOADER_COMPRESS_ZSTD
 
 #BeagleBoard.org
 ./scripts/config --enable CONFIG_MSPM0_I2C
