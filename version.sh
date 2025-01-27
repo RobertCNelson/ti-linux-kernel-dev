10be02aea34212f14e2a3c628d1218ec61c297e1
Robert Nelson <robertcnelson@gmail.com>
2025-01-27 13:27:55 -0600

    fixup: scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -56,7 +56,7 @@ KERNEL_TAG=${KERNEL_REL}.83
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.1/
 kernel_rt=".83-rt28"
 #Kernel Build
-BUILD=${build_prefix}65
+BUILD=${build_prefix}65.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
