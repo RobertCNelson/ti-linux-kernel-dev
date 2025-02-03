32a2306a800635513d3bd09404ca871ad1ec7852
Robert Nelson <robertcnelson@gmail.com>
2025-02-03 15:34:01 -0600

    Merge TI Branch; https://github.com/RobertCNelson/ti-linux-kernel/compare/ade72d105eb7c9a2a4addfd0204eb4551b8efa7a...8906665ace32eb0dc12810667ee8646bd660c374 TI SDK: 11.00.01 BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/390a6bcef8a38c7adb643f6bc8d2d5e81c21fa20 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=f67f40d009199ee75c906f1ac898b38be981a7f7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -377,6 +377,16 @@ config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 ./scripts/config --module CONFIG_OPT4060
 ./scripts/config --enable CONFIG_FPROBE
 
+#TI: 11.00.01
+./scripts/config --enable CONFIG_SRAM_DMA_HEAP
+./scripts/config --module CONFIG_CC33XX=m
+./scripts/config --module CONFIG_CC33XX_SDIO=m
+./scripts/config --module CONFIG_VIDEO_IMX390=m
+./scripts/config --enable CONFIG_DMABUF_HEAPS
+./scripts/config --enable CONFIG_DMABUF_HEAPS_SYSTEM
+./scripts/config --enable CONFIG_DMABUF_HEAPS_CMA
+./scripts/config --enable CONFIG_DMABUF_HEAPS_CARVEOUT
+
 #configure CONFIG_EXTRA_FIRMWARE
 ./scripts/config --set-str CONFIG_EXTRA_FIRMWARE "regulatory.db regulatory.db.p7s cadence/mhdp8546.bin"
 ./scripts/config --enable CONFIG_FW_LOADER_COMPRESS
