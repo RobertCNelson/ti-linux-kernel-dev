d4d9d6537ae117cf55a352963a9505ff31607637
Robert Nelson <robertcnelson@gmail.com>
2025-02-05 13:18:20 -0600

    kernel v6.1.83-ti-rt-arm64-r66.1 rebase external git projects and rt: v6.1.83-rt28 TI SDK: 09.02.00.010 BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/4af7101cac84187b6abcc4f123692835d9c2420a RT: patch-6.1.83-rt28.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=f67f40d009199ee75c906f1ac898b38be981a7f7
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -368,6 +368,25 @@ config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 ./scripts/config --enable CONFIG_ZRAM_DEF_COMP_LZ4
 ./scripts/config --set-str CONFIG_ZRAM_DEF_COMP "lz4"
 
+#new in v6.14
+./scripts/config --module CONFIG_NTSYNC
+./scripts/config --module CONFIG_PPS_GENERATOR
+./scripts/config --module CONFIG_SENSORS_CRPS
+./scripts/config --module CONFIG_SENSORS_TPS25990
+./scripts/config --module CONFIG_BD79703
+./scripts/config --module CONFIG_OPT4060
+./scripts/config --enable CONFIG_FPROBE
+
+#TI: 11.00.01
+./scripts/config --enable CONFIG_SRAM_DMA_HEAP
+./scripts/config --module CONFIG_CC33XX
+./scripts/config --module CONFIG_CC33XX_SDIO
+./scripts/config --module CONFIG_VIDEO_IMX390
+./scripts/config --enable CONFIG_DMABUF_HEAPS
+./scripts/config --enable CONFIG_DMABUF_HEAPS_SYSTEM
+./scripts/config --enable CONFIG_DMABUF_HEAPS_CMA
+./scripts/config --enable CONFIG_DMABUF_HEAPS_CARVEOUT
+
 #configure CONFIG_EXTRA_FIRMWARE
 ./scripts/config --set-str CONFIG_EXTRA_FIRMWARE "regulatory.db regulatory.db.p7s cadence/mhdp8546.bin"
 ./scripts/config --enable CONFIG_FW_LOADER_COMPRESS
@@ -376,5 +395,9 @@ config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 
 #BeagleBoard.org
 ./scripts/config --enable CONFIG_MSPM0_I2C
+
+#Rust
+./scripts/config --disable CONFIG_MODVERSIONS
+./scripts/config --enable CONFIG_RUST
 
 cd ${DIR}/
