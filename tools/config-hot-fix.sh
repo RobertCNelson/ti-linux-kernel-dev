ec6491344c907adf2201df1a1a134ee5f5d7dfbd
Robert Nelson <robertcnelson@gmail.com>
2025-02-06 20:46:44 -0600

    config: drop CPU_IDLE no k3 driver
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -403,10 +403,6 @@ config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 ./scripts/config --enable CONFIG_FW_LOADER_COMPRESS_XZ
 ./scripts/config --enable CONFIG_FW_LOADER_COMPRESS_ZSTD
 
-#Cpu idle
-./scripts/config --enable CONFIG_CPU_IDLE
-./scripts/config --enable CONFIG_ARM_PSCI_CPUIDLE
-
 #BeagleBoard.org
 ./scripts/config --enable CONFIG_MSPM0_I2C
 
