eedf095a7c81c20ff57a9770b85afc2e9ada6f21
Robert Nelson <robertcnelson@gmail.com>
2025-02-14 11:19:49 -0600

    update WIRELESS_REGDB
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -56,7 +56,7 @@ KERNEL_TAG=${KERNEL_REL}.9
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".8-rt8"
 #Kernel Build
-BUILD=${build_prefix}20.1
+BUILD=${build_prefix}20.2
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
