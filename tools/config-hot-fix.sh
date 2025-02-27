d3706c634069b09d2034997cae85cde26ff4369a
Robert Nelson <robertcnelson@gmail.com>
2025-02-27 09:00:56 -0600

    config: merge in debian 6.12.16-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -374,6 +374,12 @@ config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 ./scripts/config --enable CONFIG_ZRAM_DEF_COMP_LZ4
 ./scripts/config --set-str CONFIG_ZRAM_DEF_COMP "lz4"
 
+#debian 6.12.16-1
+./scripts/config --enable CONFIG_RCU_LAZY
+./scripts/config --module CONFIG_NSM
+./scripts/config --module CONFIG_NITRO_ENCLAVES
+./scripts/config --module CONFIG_USB_MASS_STORAGE
+
 #new in v6.14
 ./scripts/config --module CONFIG_NTSYNC
 ./scripts/config --module CONFIG_PPS_GENERATOR
@@ -411,6 +417,7 @@ config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 
 #BeagleBoard.org
 ./scripts/config --enable CONFIG_MSPM0_I2C
+./scripts/config --module CONFIG_SEG_LED_GPIO
 
 #Rust
 ./scripts/config --disable CONFIG_MODVERSIONS
