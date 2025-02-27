17ffb121afc518a6052242d5cc6180d851d5282f
Robert Nelson <robertcnelson@gmail.com>
2025-02-27 10:26:55 -0600

    merge back PocketBeagle2 changes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -275,7 +275,7 @@ config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 ./scripts/config --module CONFIG_VIDEO_IMX390
 ./scripts/config --module CONFIG_VIDEO_OX05B1S
 
-#enable MIKROBUS
+#enable SPI/W1
 ./scripts/config --enable CONFIG_SPI_OMAP24XX
 ./scripts/config --enable CONFIG_W1
 ./scripts/config --enable CONFIG_MIKROBUS
@@ -287,9 +287,11 @@ config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 ./scripts/config --enable CONFIG_FUNCTION_TRACER
 ./scripts/config --enable CONFIG_DYNAMIC_FTRACE
 
+./scripts/config --enable CONFIG_MODULE_COMPRESS
 ./scripts/config --disable CONFIG_MODULE_COMPRESS_GZIP
 ./scripts/config --enable CONFIG_MODULE_COMPRESS_XZ
 ./scripts/config --disable CONFIG_MODULE_COMPRESS_ZSTD
+./scripts/config --enable CONFIG_MODULE_COMPRESS_ALL
 ./scripts/config --enable CONFIG_GPIO_AGGREGATOR
 ./scripts/config --module CONFIG_PWM_GPIO
 
@@ -314,17 +316,25 @@ config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 #PCI Express Precision Time Measurement support
 ./scripts/config --enable CONFIG_PCIE_PTM
 
+#REMOTEPROC
 ./scripts/config --module CONFIG_RPMSG
 ./scripts/config --module CONFIG_RPMSG_NS
-./scripts/config --module CONFIG_RPMSG_VIRTIO
+./scripts/config --module CONFIG_RPMSG_PRU
+./scripts/config --enable CONFIG_RPMSG_VIRTIO
+./scripts/config --module CONFIG_TI_K3_DSP_REMOTEPROC
+./scripts/config --module CONFIG_TI_K3_M4_REMOTEPROC
+./scripts/config --module CONFIG_TI_K3_R5_REMOTEPROC
 
 #Google Coral Gasket
 ./scripts/config --module CONFIG_STAGING_GASKET_FRAMEWORK
 ./scripts/config --module CONFIG_STAGING_APEX_DRIVER
 
+#DRM_PANIC
+./scripts/config --enable CONFIG_DRM_PANIC
+./scripts/config --enable CONFIG_DRM_PANIC_SCREEN_QR_CODE
+
 #TI: 10.00.04
 ./scripts/config --enable CONFIG_FB_SIMPLE
-./scripts/config --module CONFIG_RPMSG_PRU
 
 #TI: 10.00.06
 ./scripts/config --disable CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL
@@ -337,8 +347,6 @@ config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 #new in v6.12.x
 ./scripts/config --enable CONFIG_PREEMPT_RT
 ./scripts/config --enable CONFIG_RPMB
-./scripts/config --enable CONFIG_DRM_PANIC
-./scripts/config --module CONFIG_TI_K3_M4_REMOTEPROC
 ./scripts/config --module CONFIG_ADXL380_SPI
 ./scripts/config --module CONFIG_ADXL380_I2C
 ./scripts/config --module CONFIG_AD4000
@@ -365,6 +373,12 @@ config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 ./scripts/config --enable CONFIG_ZRAM_BACKEND_DEFLATE
 ./scripts/config --enable CONFIG_ZRAM_DEF_COMP_LZ4
 ./scripts/config --set-str CONFIG_ZRAM_DEF_COMP "lz4"
+
+#debian 6.12.16-1
+./scripts/config --enable CONFIG_RCU_LAZY
+./scripts/config --module CONFIG_NSM
+./scripts/config --module CONFIG_NITRO_ENCLAVES
+./scripts/config --module CONFIG_USB_MASS_STORAGE
 
 #new in v6.14
 ./scripts/config --module CONFIG_NTSYNC
@@ -395,18 +409,23 @@ config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 #./scripts/config --module CONFIG_PHY_CADENCE_DPHY
 ./scripts/config --module CONFIG_TI_ECAP_CAPTURE
 
+#TI: 11.00.04
+./scripts/config --enable CONFIG_MTD_SPI_NAND
+./scripts/config --enable CONFIG_MTD_UBI
+./scripts/config --enable CONFIG_TI_K3_UDMA_AM62L
+./scripts/config --enable CONFIG_UBIFS_FS
+./scripts/config --enable CONFIG_CRYPTO_ZSTD
+./scripts/config --enable CONFIG_ZSTD_COMPRESS
+
 #configure CONFIG_EXTRA_FIRMWARE
 ./scripts/config --set-str CONFIG_EXTRA_FIRMWARE "regulatory.db regulatory.db.p7s cadence/mhdp8546.bin"
 ./scripts/config --enable CONFIG_FW_LOADER_COMPRESS
 ./scripts/config --enable CONFIG_FW_LOADER_COMPRESS_XZ
 ./scripts/config --enable CONFIG_FW_LOADER_COMPRESS_ZSTD
 
-#Cpu idle
-./scripts/config --enable CONFIG_CPU_IDLE
-./scripts/config --enable CONFIG_ARM_PSCI_CPUIDLE
-
 #BeagleBoard.org
 ./scripts/config --enable CONFIG_MSPM0_I2C
+./scripts/config --module CONFIG_SEG_LED_GPIO
 
 #Rust
 ./scripts/config --disable CONFIG_MODVERSIONS
