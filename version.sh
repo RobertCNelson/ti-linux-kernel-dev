fee4376bc36e4ccfa535f3a124c9ddd86048df50
Robert Nelson <robertcnelson@gmail.com>
2025-03-21 19:08:05 -0500

    cleanup: sync with v6.14.x fix beagley-ai ethernet
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -56,7 +56,7 @@ KERNEL_TAG=${KERNEL_REL}.58
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.6/
 kernel_rt=".58-rt45"
 #Kernel Build
-BUILD=${build_prefix}23.2
+BUILD=${build_prefix}23.3
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
