774a8d8ce69e059250a5df60335b78ee7cfdbdef
Robert Nelson <robertcnelson@gmail.com>
2025-03-21 19:03:47 -0500

    cleanup: beagley-ai
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -56,7 +56,7 @@ KERNEL_TAG=${KERNEL_REL}.17
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".8-rt8"
 #Kernel Build
-BUILD=${build_prefix}28.4
+BUILD=${build_prefix}28.5
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
