ef5e0c8b9b6f4a996aa73ba9f178acefba4e0b79
Robert Nelson <robertcnelson@gmail.com>
2025-04-11 15:55:05 -0500

    6.12.17-ti-arm32-r8 release BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/dd3342c2a3e6df6ee02eaa7083d3ad66bd3bfe82 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=6953f19b96f4bf21b22de5c039e372bb33e2c96b
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -51,7 +51,7 @@ KERNEL_TAG=${KERNEL_REL}.17
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".8-rt8"
 #Kernel Build
-BUILD=${build_prefix}7.3
+BUILD=${build_prefix}8
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
