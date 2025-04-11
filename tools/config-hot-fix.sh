ed21a96abc6394fba3a386da32a2507cf8b7e70b
Robert Nelson <robertcnelson@gmail.com>
2025-04-11 15:42:13 -0500

    config: enable CONFIG_INPUT_PWM_BEEPER
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -473,6 +473,7 @@ config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 #BeagleBoard.org
 ./scripts/config --enable CONFIG_MSPM0_I2C
 ./scripts/config --module CONFIG_SEG_LED_GPIO
+./scripts/config --module CONFIG_INPUT_PWM_BEEPER
 ./scripts/config --module CONFIG_SND_SOC_TLV320AIC3X_I2C
 ./scripts/config --module CONFIG_WIZNET_W5100
 ./scripts/config --module CONFIG_WIZNET_W5100_SPI
