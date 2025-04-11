8d1bb64b67b38090d924f05b57307702d9f34865
Robert Nelson <robertcnelson@gmail.com>
2025-04-11 17:11:39 -0500

    kernel v6.6.58-ti-arm32-r10.2 rebase external git projects TI SDK: 10.01.10 BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/8e4577364ab257da9afac6bfc25cf9327b029df8 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=6953f19b96f4bf21b22de5c039e372bb33e2c96b
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -632,8 +632,6 @@ drivers () {
 	dir 'external/android'
 	dir 'external/ti-amx3-cm3-pm-firmware'
 
-	#git revert --no-edit -s 3edf588e7fe00e90d1dc7fb9e599861b2c2cf442
-	#Breaking Kingston eMMC on new BBB's..
 	dir 'drivers/fixes/mmc'
 }
 
