d7cb419195d2a4532beaed78d0b28cf57d4d2d9a
Robert Nelson <robertcnelson@gmail.com>
2025-04-14 12:16:03 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- tools/config-hot-fix.sh
+++ tools/config-hot-fix.sh
@@ -394,6 +394,9 @@ config="CONFIG_UIO_PDRV_GENIRQ" ; config_module
 ./scripts/config --module CONFIG_NVME_TARGET_LOOP
 ./scripts/config --module CONFIG_NVME_TARGET_FCLOOP
 
+#debian 6.13.11-1
+./scripts/config --enable CONFIG_KALLSYMS_ALL
+
 #new in v6.14
 ./scripts/config --module CONFIG_NTSYNC
 ./scripts/config --module CONFIG_PPS_GENERATOR
