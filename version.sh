94af84444402d0a6be2cef8dfe10f0953f53ea69
Robert Nelson <robertcnelson@gmail.com>
2025-04-14 13:10:15 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -13,8 +13,6 @@ bborg_branch="5.10"
 #arm
 KERNEL_ARCH=arm
 DEBARCH=armhf
-#toolchain="gcc_6_arm"
-#toolchain="gcc_7_arm"
 #toolchain="gcc_8_arm"
 #toolchain="gcc_9_arm"
 toolchain="gcc_10_arm"
@@ -25,8 +23,6 @@ toolchain="gcc_10_arm"
 #arm64
 #KERNEL_ARCH=arm64
 #DEBARCH=arm64
-#toolchain="gcc_6_aarch64"
-#toolchain="gcc_7_aarch64"
 #toolchain="gcc_8_aarch64"
 #toolchain="gcc_9_aarch64"
 #toolchain="gcc_10_aarch64"
@@ -37,7 +33,6 @@ toolchain="gcc_10_arm"
 #riscv64
 #KERNEL_ARCH=riscv
 #DEBARCH=riscv64
-#toolchain="gcc_7_riscv64"
 #toolchain="gcc_8_riscv64"
 #toolchain="gcc_9_riscv64"
 #toolchain="gcc_10_riscv64"
