e2f3d7bc7470947bd7ccf2d1f6527e19a78bce44
Robert Nelson <robertcnelson@gmail.com>
2025-04-24 19:02:17 -0500

    add: am62-pocketbeagle2 and am335x-boneblack-revd
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -123,6 +123,12 @@ external_git () {
 	#exit 2
 }
 
+mainline_patches () {
+	#exit 2
+	dir 'mainline/pocketbeagle2'
+	#exit 2
+}
+
 rt_cleanup () {
 	echo "rt: needs fixup"
 	exit 2
@@ -253,6 +259,7 @@ k3_makefile_patch_cleanup_overlays () {
 	echo "# Enable support for device-tree overlays" >> arch/arm64/boot/dts/ti/Makefile
 	cat arch/arm64/boot/dts/ti/Makefile.dtc >> arch/arm64/boot/dts/ti/Makefile
 	rm arch/arm64/boot/dts/ti/Makefile.dtc
+	echo "DTC_FLAGS_k3-am62-pocketbeagle2 += -@" >> arch/arm64/boot/dts/ti/Makefile
 	echo "DTC_FLAGS_k3-am6232-pocketbeagle2 += -@" >> arch/arm64/boot/dts/ti/Makefile
 	echo "DTC_FLAGS_k3-am67a-beagley-ai += -@" >> arch/arm64/boot/dts/ti/Makefile
 	echo "DTC_FLAGS_k3-j721e-beagleboneai64 += -@" >> arch/arm64/boot/dts/ti/Makefile
@@ -322,6 +329,7 @@ beagleboard_dtbs () {
 		device="PB-MIKROBUS-1" ; arm_dtbo_makefile_append
 
 		device="am335x-boneblack-uboot.dtb" ; arm_dtb_makefile_append
+		device="am335x-boneblack-revd.dtb" ; arm_dtb_makefile_append
 
 		#device="am335x-sancloud-bbe-uboot.dtb" ; arm_dtb_makefile_append
 		#device="am335x-sancloud-bbe-lite-uboot.dtb" ; arm_dtb_makefile_append
@@ -450,6 +458,7 @@ local_patch () {
 }
 
 external_git
+mainline_patches
 rt
 wireless_regdb
 beagleboard_dtbs
