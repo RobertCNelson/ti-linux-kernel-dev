d34938177dda3ff4047bf398e0e2213d1e33f183
Robert Nelson <robertcnelson@gmail.com>
2025-05-01 19:14:36 -0500

    ci: build just zImage/modules
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -35,6 +35,10 @@ toolchain="gcc_linaro_gnueabihf_5"
 #toolchain="gcc_arm_aarch64_gnu_9"
 
 #Kernel
+linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/linux.git"
+#linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
+linux_stable_repo="https://github.com/beagleboard/mirror-ti-linux-kernel.git"
+#
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.155
 kernel_rt=".157-rt174"
