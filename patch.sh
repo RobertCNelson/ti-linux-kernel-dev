abbeef28ec91d54477cfdd1c020d817246579b70
Robert Nelson <robertcnelson@gmail.com>
2025-05-20 18:32:40 -0500

    Merge TI Branch; https://github.com/RobertCNelson/ti-linux-kernel/compare/598740560225b2d404329bf466fb10475934de23...013211ccdf5ec33e19652be631b820bc5f6b86a4 TI SDK: 11.00.13 BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/dca2d1f5b10188674c165d685a3686edea516ab6 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=6953f19b96f4bf21b22de5c039e372bb33e2c96b
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -348,6 +348,7 @@ beagleboard_dtbs () {
 		#ls src/arm64/overlays/ | grep pocketbeagle2
 
 		device="k3-am6232-pocketbeagle2-techlab-cape" ; k3_dtbo_makefile_append
+		device="k3-am62-pocketbeagle2-leds-off" ; k3_dtbo_makefile_append
 
 		#ls src/arm64/overlays/ | grep beaglemod
 
@@ -554,8 +555,6 @@ drivers () {
 	dir 'external/android'
 	dir 'external/cadence'
 	dir 'external/gasket'
-
-	dir 'drivers/fixes/mmc'
 }
 
 ###
--- version.sh
+++ version.sh
@@ -53,11 +53,11 @@ linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/li
 linux_stable_repo="https://github.com/beagleboard/mirror-ti-linux-kernel.git"
 #
 KERNEL_REL=6.12
-KERNEL_TAG=${KERNEL_REL}.22
+KERNEL_TAG=${KERNEL_REL}.23
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".16-rt9"
 #Kernel Build
-BUILD=${build_prefix}35
+BUILD=${build_prefix}35.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
@@ -68,8 +68,8 @@ BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 
 DISTRO=xross
 
-sdk_git_old_release="95010a7c186b86f738615231016aa017ac527612"
-sdk_git_new_release="598740560225b2d404329bf466fb10475934de23"
-SDK="11.00.12"
+sdk_git_old_release="598740560225b2d404329bf466fb10475934de23"
+sdk_git_new_release="013211ccdf5ec33e19652be631b820bc5f6b86a4"
+SDK="11.00.13"
 
 #
