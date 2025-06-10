1945f3692e2412ac84da364ec1348b53f5381817
Robert Nelson <robertcnelson@gmail.com>
2025-06-10 14:54:33 -0500

    debian: 6.12.30-1
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patches/get_debian_configs.sh
+++ patches/get_debian_configs.sh
@@ -3,11 +3,8 @@
 #
 #https://packages.debian.org/source/trixie/linux
 #
-abi="6.12.21"
-kernel="6.12.21-1"
-#
-#abi="6.13-rc7"
-#kernel="6.13~rc7-1~exp1"
+abi="6.12.30"
+kernel="6.12.30-1"
 #
 
 mirror_site="http://192.168.1.10/debian/pool/main/l/linux"
