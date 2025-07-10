6d648b59bfd5039a614dfbe9653319b4e6885024
Robert Nelson <robertcnelson@gmail.com>
2025-07-10 14:21:48 -0500

    Merge TI Branch; https://github.com/RobertCNelson/ti-linux-kernel/compare/251ed68a96e3f85a5db174c9c39dc334c0f0ba5f...34d0b070130a8cd2cddd9260389ad974e8cb8468 TI SDK: 11.01.03 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/f45066b9028804189770d7e9201739c024bd87d3 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=6953f19b96f4bf21b22de5c039e372bb33e2c96b
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -57,7 +57,7 @@ KERNEL_TAG=${KERNEL_REL}.33
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".16-rt9"
 #Kernel Build
-BUILD=${build_prefix}43.3
+BUILD=${build_prefix}43.4
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
@@ -68,8 +68,8 @@ BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
 
 DISTRO=xross
 
-sdk_git_old_release="20778f6c125dc24e62767dc4837d793a0f18c319"
-sdk_git_new_release="251ed68a96e3f85a5db174c9c39dc334c0f0ba5f"
-SDK="11.01.02"
+sdk_git_old_release="251ed68a96e3f85a5db174c9c39dc334c0f0ba5f"
+sdk_git_new_release="34d0b070130a8cd2cddd9260389ad974e8cb8468"
+SDK="11.01.03"
 
 #
