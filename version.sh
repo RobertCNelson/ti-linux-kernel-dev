ec4b7b1dccc8d08418b7866e4d015c2facdbe290
Robert Nelson <robertcnelson@gmail.com>
2025-08-07 13:34:40 -0500

    kernel v5.10.168-ti-arm64-r118.1 rebase external git projects TI SDK: 08.06.00.007 BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/100884db5ca038af3a9c56c6142aa2067662218a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=e05260afb7203dd0a7ba6faf9a30c86faf19a24e BCFSERIAL: https://openbeagle.org/beagleconnect/linux/bcfserial/-/commit/db467023bd136c97c2e13c3a8b9e41dbdfafbc66
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -57,7 +57,7 @@ KERNEL_TAG=${KERNEL_REL}.168
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/5.10/
 kernel_rt=".168-rt83"
 #Kernel Build
-BUILD=${build_prefix}118
+BUILD=${build_prefix}118.1
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
@@ -80,7 +80,4 @@ SDK="08.06.00.007"
 #
 #https://git.ti.com/gitweb?p=ti-linux-kernel/ti-linux-kernel.git;a=tag;h=refs/tags/08.06.00.005
 #https://git.ti.com/gitweb?p=ti-linux-kernel/ti-linux-kernel.git;a=commit;h=73f2f2bba715b5cada420f2fb34bb43d185248f2
-#
-#https://git.ti.com/gitweb?p=ti-linux-kernel/ti-linux-kernel.git;a=tag;h=refs/tags/08.06.00.004
-#https://git.ti.com/gitweb?p=ti-linux-kernel/ti-linux-kernel.git;a=commit;h=2927372e2c7cc3782c07b1896757962bc346d4c5
 #
