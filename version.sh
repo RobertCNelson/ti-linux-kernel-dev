a235a74b2eae0e879e3da0fece5a034fa0316079
Robert Nelson <robertcnelson@gmail.com>
2025-08-27 14:16:58 -0500

    6.12.42-ti-arm64-r51 release BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/b17a49721b9620a36785836b3cfdce92be341610 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=e05260afb7203dd0a7ba6faf9a30c86faf19a24e
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -57,7 +57,7 @@ KERNEL_TAG=${KERNEL_REL}.42
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.12/
 kernel_rt=".39-rt11"
 #Kernel Build
-BUILD=${build_prefix}50.3
+BUILD=${build_prefix}51
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
