c71fd943349c7a82ca9853acd89ca9341e6e06fb
Robert Nelson <robertcnelson@gmail.com>
2025-11-01 23:15:38 -0500

    undo tps65219 ti changes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -566,7 +566,7 @@ backports () {
 		cp -v ~/linux-src/include/linux/mfd/tps65219.h ./include/linux/mfd/
 
 		post_backports
-	#else
+	else
 		patch_backports
 	fi
 
@@ -598,7 +598,7 @@ drivers () {
 	dir 'external/cadence'
 	dir 'external/gasket'
 
-	#dir 'drivers/tps65219'
+	dir 'drivers/tps65219'
 	dir 'drivers/configfs'
 }
 
