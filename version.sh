f703e983a2322cb9437f3989a3b4e10258d77925
Robert Nelson <robertcnelson@gmail.com>
2025-12-05 13:17:04 -0600

    scripts: grab tag from stable branch
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -53,8 +53,7 @@ WIRELESS_REGDB="2025.10.07"
 
 #Kernel
 linux_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/torvalds/linux.git"
-#linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
-linux_stable_repo="https://github.com/beagleboard/mirror-ti-linux-kernel.git"
+linux_stable_repo="https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git"
 #
 KERNEL_REL=6.12
 KERNEL_TAG=${KERNEL_REL}.57
